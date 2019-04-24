afa1721991bb66fa2865b613a88c72831bb019a6
Simone Tiraboschi <stirabos@redhat.com>
2019-04-24 18:57:51 +0200

    Rebase on current code

--- hack/ds_test.sh
+++ hack/ds_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 # Create the namespace for the HCO
-oc create ns kubevirt-hyperconverged
+oc create ns kubevirt-hyperconverged || true
 
 # Create an OperatorGroup
 cat <<EOF | oc create -f -
@@ -14,19 +14,28 @@ metadata:
   namespace: kubevirt-hyperconverged
 EOF
 
-# Create a Catalog Source
+# Create a Catalog Source backed by a grpc registry
+#
+# Notes for QE:
+#   - This is going to be replaced by https://quay.io/application/redhat-operators-stage/kubevirt-hyperconverged in the future
+#
 cat <<EOF | oc create -f -
 apiVersion: operators.coreos.com/v1alpha1
 kind: CatalogSource
 metadata:
   name: hco-catalogsource
   namespace: openshift-operator-lifecycle-manager
+  imagePullPolicy: Always
 spec:
   sourceType: grpc
-  image: brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888/container-native-virtualization/hyperconverged-cluster-operator:v2.0.0-4
+  image: docker.io/rthallisey/hco-registry:qe-1.6
   displayName: KubeVirt HyperConverged
   publisher: Red Hat
 EOF
+
+WAIT_TIMEOUT="360s"
+echo "Waiting up to ${WAIT_TIMEOUT} for catalogsource to appear..."
+oc wait pod $(oc get pods -n openshift-operator-lifecycle-manager | grep hco-catalogsource | awk '{print $1}') --for condition=Ready -n openshift-operator-lifecycle-manager --timeout="${WAIT_TIMEOUT}"
 
 # Create a subscription
 cat <<EOF | oc create -f -
@@ -41,3 +50,23 @@ spec:
   source: hco-catalogsource
   sourceNamespace: openshift-operator-lifecycle-manager
 EOF
+
+echo "Give the operators some time to start..."
+sleep 30
+
+VIRT_POD=`oc get pods -n kubevirt-hyperconverged | grep virt-operator | head -1 | awk '{ print $1 }'`
+CDI_POD=`oc get pods -n kubevirt-hyperconverged | grep cdi-operator | head -1 | awk '{ print $1 }'`
+NETWORK_ADDONS_POD=`oc get pods -n kubevirt-hyperconverged | grep cluster-network-addons-operator | head -1 | awk '{ print $1 }'`
+oc wait pod $VIRT_POD --for condition=Ready -n kubevirt-hyperconverged --timeout="${WAIT_TIMEOUT}"
+oc wait pod $CDI_POD --for condition=Ready -n kubevirt-hyperconverged --timeout="${WAIT_TIMEOUT}"
+oc wait pod $NETWORK_ADDONS_POD --for condition=Ready -n kubevirt-hyperconverged --timeout="${WAIT_TIMEOUT}"
+
+echo "Launching CNV..."
+cat <<EOF | oc create -f -
+apiVersion: hco.kubevirt.io/v1alpha1
+kind: HyperConverged
+metadata:
+  name: hyperconverged-cluster
+  namespace: kubevirt-hyperconverged
+EOF
+
