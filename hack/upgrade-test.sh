c95b0b480cbe58a8c904e3f1672a72f69926a5e0
Richard Su <rwsu@redhat.com>
2019-08-29 15:17:43 -0700

    Add debugging info, description, and license

--- hack/upgrade-test.sh
+++ hack/upgrade-test.sh
@@ -1,17 +1,58 @@
 #!/bin/bash -e
 #
+# This file is part of the KubeVirt project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Copyright 2017 Red Hat, Inc.
+#
 # Usage:
-# export KUBEVIRT_PROVIDER=okd-4.1.0
+# export KUBEVIRT_PROVIDER=okd-4.1
 # make cluster-up
 # make upgrade-test
+#
+# Deploys the HCO cluster using the latest version in the git repo.
+# This latest version deploys the hco-operator using the :latest tag 
+# on quay.io.
+#
+# A new version, named 100.0.0, is then created. A new hco-operator
+# image is created based off of the code in the current checkout.  
+# A CSV and registry image is created for this version. The CSV
+# uses the new hco-operator image in the hco deployment.
+#
+# Both the hco-operator image and new registry image is pushed
+# to the local registry.
+#
+# The hco-catalogsource pod is then patched to use the new registry
+# image.
+#
+# The subscription is checked to verify that it progresses
+# to the new version. 
+# 
+# The hyperconverged-cluster deployment's image is also checked
+# to verify that it is updated to the new operator image from 
+# the local registry.
+
+echo "-- Upgrade Step 1/8: clean cluster"
+make cluster-clean
+
+echo "-- Upgrade Step 2/8: build registry image (tag:latest)"
 
 container_id=$(docker ps | grep kubevirtci | cut -d ' ' -f 1)
 registry_port=$(docker port $container_id | grep 5000 | cut -d ':' -f 2)
 registry=localhost:$registry_port
 
 echo "INFO: registry: $registry"
-
-make cluster-clean
 
 export REGISTRY_NAMESPACE=kubevirt
 export IMAGE_REGISTRY=$registry
@@ -29,6 +70,8 @@ done
 
 ./cluster-up/kubectl.sh wait deployment packageserver --for condition=Available -n openshift-operator-lifecycle-manager --timeout="1200s"
 ./cluster-up/kubectl.sh wait deployment catalog-operator --for condition=Available -n openshift-operator-lifecycle-manager --timeout="1200s"
+
+echo "-- Upgrade Step 3/8: create catalogsource and subscription to install HCO"
 
 ./cluster-up/kubectl.sh create ns kubevirt-hyperconverged | true
 
@@ -110,6 +153,9 @@ echo "----- Images before upgrade"
 
 LATEST_VERSION=`ls -d ./deploy/olm-catalog/kubevirt-hyperconverged/*/ | sort -r | head -1 | cut -d '/' -f 5`
 UPGRADE_VERSION=100.0.0
+
+echo "-- Upgrade Step 4/8: create version $UPGRADE_VERSION, the target for upgrade"
+
 cp -r ./deploy/olm-catalog/kubevirt-hyperconverged/$LATEST_VERSION ./deploy/olm-catalog/kubevirt-hyperconverged/$UPGRADE_VERSION
 
 mv ./deploy/olm-catalog/kubevirt-hyperconverged/$UPGRADE_VERSION/kubevirt-hyperconverged-operator.v$LATEST_VERSION.clusterserviceversion.yaml ./deploy/olm-catalog/kubevirt-hyperconverged/$UPGRADE_VERSION/kubevirt-hyperconverged-operator.v$UPGRADE_VERSION.clusterserviceversion.yaml
@@ -120,6 +166,8 @@ sed -i "s|  version: $LATEST_VERSION|  version: $UPGRADE_VERSION|g" ./deploy/olm
 sed -i "s|quay.io/kubevirt/hyperconverged-cluster-operator:latest|registry:5000/kubevirt/hyperconverged-cluster-operator:latest|g" ./deploy/olm-catalog/kubevirt-hyperconverged/$UPGRADE_VERSION/kubevirt-hyperconverged-operator.v$UPGRADE_VERSION.clusterserviceversion.yaml
 
 sed -i "s|currentCSV: kubevirt-hyperconverged-operator.v$LATEST_VERSION|currentCSV: kubevirt-hyperconverged-operator.v$UPGRADE_VERSION|g" ./deploy/olm-catalog/kubevirt-hyperconverged/kubevirt-hyperconverged.package.yaml
+
+echo "-- Upgrade Step 5/8: build new HCO operator image and HCO registry image (tag:upgrade) and push to local registry"
 
 # Build a new registry image for the new version.
 export CONTAINER_TAG=upgrade
@@ -133,15 +181,15 @@ for NODE in $CLUSTER_NODES; do
     ./cluster-up/ssh.sh $NODE 'sudo sysctl -w user.max_user_namespaces=1024'
 done
 
+echo "-- Upgrade Step 6/8: patch existing catalog source with new registry image"
+echo "-- and wait for hco-catalogsource pod to be in Ready state"
+
 # Patch the HCO catalogsource image to the upgrade version
 ./cluster-up/kubectl.sh patch catalogsource hco-catalogsource-example -n openshift-operator-lifecycle-manager -p '{"spec":{"image": "registry:5000/kubevirt/hco-registry:upgrade"}}' --type merge
 ./hack/retry.sh 20 30 "./cluster-up/kubectl.sh get pods -n openshift-operator-lifecycle-manager | grep hco-catalogsource"
 HCO_CATALOGSOURCE_POD=`./cluster-up/kubectl.sh get pods -n openshift-operator-lifecycle-manager | grep hco-catalogsource | head -1 | awk '{ print $1 }'`
 ./cluster-up/kubectl.sh wait pod $HCO_CATALOGSOURCE_POD --for condition=Ready -n openshift-operator-lifecycle-manager --timeout="120s"
 
-# Delete the catalog-operator to force it to reload and read the 
-# new HCO catalogsource. Otherwise it could take up to 10 mins
-# for it to detect that the catalogsource has been updated.
 sleep 15
 CATALOG_OPERATOR_POD=`./cluster-up/kubectl.sh get pods -n openshift-operator-lifecycle-manager | grep catalog-operator | head -1 | awk '{ print $1 }'`
 ./cluster-up/kubectl.sh wait pod $CATALOG_OPERATOR_POD --for condition=Ready -n openshift-operator-lifecycle-manager --timeout="120s"
@@ -149,6 +197,7 @@ CATALOG_OPERATOR_POD=`./cluster-up/kubectl.sh get pods -n openshift-operator-lif
 # Verify the subscription has changed to the new version
 #  currentCSV: kubevirt-hyperconverged-operator.v100.0.0
 #  installedCSV: kubevirt-hyperconverged-operator.v100.0.0
+echo "-- Upgrade Step 7/8: verify the subscription's currentCSV and installedCSV have moved to the new version"
 sleep 10
 HCO_OPERATOR_POD=`./cluster-up/kubectl.sh get pods -n kubevirt-hyperconverged | grep hco-operator | head -1 | awk '{ print $1 }'`
 ./cluster-up/kubectl.sh wait pod $HCO_OPERATOR_POD --for condition=Ready -n kubevirt-hyperconverged --timeout="600s"
@@ -157,6 +206,7 @@ HCO_OPERATOR_POD=`./cluster-up/kubectl.sh get pods -n kubevirt-hyperconverged | 
 
 # Verify hco-operator has updated to the new version from the local registry
 # registry:5000/kubevirt/hyperconverged-cluster-operator:latest
+echo "-- Upgrade Step 8/8: verify the hyperconverged-cluster deployment is using the new image from local registry"
 ./hack/retry.sh 2 30 "./cluster-up/kubectl.sh get deployments -n kubevirt-hyperconverged -o yaml | grep image | grep hyperconverged-cluster | grep registry:5000"
 
 echo "----- Images after upgrade"
