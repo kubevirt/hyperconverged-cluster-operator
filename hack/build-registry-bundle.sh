b47e80459db1de77023846ff7672505e91421049
Richard Su <rwsu@redhat.com>
2019-09-16 15:18:25 -0700

    test image build and push

--- hack/build-registry-bundle.sh
+++ hack/build-registry-bundle.sh
@@ -4,6 +4,7 @@ IMAGE_REGISTRY="${IMAGE_REGISTRY:-docker.io}"
 REGISTRY_NAMESPACE="${REGISTRY_NAMESPACE:-}"
 CONTAINER_TAG="${CONTAINER_TAG:-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)}"
 CONTAINER_BUILD_CMD="${CONTAINER_BUILD_CMD:-docker}"
+CONTAINER_BUILD_EXTRA_FLAGS="${CONTAINER_BUILD_EXTRA_FLAGS:-}"
 
 if [ -z "${REGISTRY_NAMESPACE}" ]; then
     echo "Please set REGISTRY_NAMESPACE"
@@ -16,6 +17,6 @@ TMP_ROOT="$(dirname "${BASH_SOURCE[@]}")/.."
 REPO_ROOT=$(readlink -e "${TMP_ROOT}" 2> /dev/null || perl -MCwd -e 'print Cwd::abs_path shift' "${TMP_ROOT}")
 
 pushd ${REPO_ROOT}/deploy
-$CONTAINER_BUILD_CMD build --no-cache -t ${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/hco-registry:${CONTAINER_TAG} -f Dockerfile .
-$CONTAINER_BUILD_CMD push ${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/hco-registry:${CONTAINER_TAG}
+$CONTAINER_BUILD_CMD build --no-cache $CONTAINER_BUILD_EXTRA_FLAGS -t ${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/hco-registry:${CONTAINER_TAG} -f Dockerfile .
+$CONTAINER_BUILD_CMD push $CONTAINER_BUILD_EXTRA_FLAGS ${IMAGE_REGISTRY}/${REGISTRY_NAMESPACE}/hco-registry:${CONTAINER_TAG}
 popd
--- hack/functest.sh
+++ hack/functest.sh
@@ -27,3 +27,31 @@ oc get routes -A
 
 cat $KUBECONFIG
 
+oc patch configs.imageregistry.operator.openshift.io/cluster --type merge -p '{"spec":{"defaultRoute":true}}'
+
+oc get routes -n openshift-image-registry -o yaml 
+
+REGISTRY_URL=`oc get routes -n openshift-image-registry | grep image-registry | tr -s ' ' | cut -d ' ' -f 2`
+
+echo $REGISTRY_URL
+
+dnf install podman -y
+sudo dnf install podman -y
+
+oc sa get-token -n openshift builder | podman login -u builder --tls-verify=false --password-stdin $REGISTRY_URL
+
+#BUILDER_TOKEN=`oc sa get-token -n openshift builder`
+#oc login --token="$BUILDER_TOKEN"
+
+oc create ns kubevirt | true
+export CONTAINER_TAG=upgrade
+export REGISTRY_NAMESPACE=kubevirt
+export IMAGE_REGISTRY=$REGISTRY_URL
+export CONTAINER_BUILD_CMD="podman"
+make bundleRegistry
+
+jq
+
+docker version
+
+rpm -qa | grep docker
