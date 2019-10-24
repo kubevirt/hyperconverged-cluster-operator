b06b7c57daf705662e7eeb6f983801c685b57ec8
Richard Su <rwsu@redhat.com>
2019-10-23 17:00:56 -0700

    Remove line used in testing
    Signed-off-by: Richard Su <rwsu@redhat.com>

--- hack/upgrade-test-build-images.sh
+++ hack/upgrade-test-build-images.sh
@@ -22,7 +22,6 @@
 container_id=$(docker ps | grep kubevirtci | cut -d ' ' -f 1)
 registry_port=$(docker port $container_id | grep 5000 | cut -d ':' -f 2)
 registry=localhost:$registry_port
-registry=default-route-openshift-image-registry.apps.rwsu.devcluster.openshift.com
 
 echo "INFO: registry: $registry"
 
