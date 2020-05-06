cda3e4ade68b9d7593e278783e06bb27eff7ff99
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2020-05-06 15:54:32 +0300

    move gotest to genkgo
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- pkg/apis/hco/v1alpha1/hyperconvarged_types_test.go
+++ pkg/apis/hco/v1alpha1/hyperconvarged_types_test.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	conditionsv1 "github.com/openshift/custom-resource-status/conditions/v1"
 	corev1 "k8s.io/api/core/v1"
 	"testing"
@@ -12,239 +14,283 @@ const (
 	testOldVersion = "anOldVersion"
 )
 
-func TestHyperConvergedStatus_UpdateVersion_noVersions(t *testing.T) {
-	hcs := &HyperConvergedStatus{Conditions: []conditionsv1.Condition{}, RelatedObjects: []corev1.ObjectReference{}}
-
-	hcs.UpdateVersion(testName, testVersion)
-
-	if len(hcs.Versions) != 1 {
-		t.Error("Should be able to add a new version to a nil version array")
-	}
-
-	if hcs.Versions[0].Name != testName {
-		t.Errorf(`Version name should be "%s" but it's "%s"`, testName, hcs.Versions[0].Name)
-	}
-
-	if hcs.Versions[0].Version != testVersion {
-		t.Errorf(`Version should be "%s" but it's "%s"`, testVersion, hcs.Versions[0].Version)
-	}
+func TestHyperConvergedStatus(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "HyperConvergedStatus Suite")
 }
 
-func TestHyperConvergedStatus_UpdateVersion_emptyVersions(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions:       Versions{},
-	}
+var _ = Describe("HyperconvergedTypes", func() {
+	Describe("HyperConvergedStatus.UpdateVersion", func() {
+		Context("Should be able to add a new version to a nil version array", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+			}
 
-	hcs.UpdateVersion(testName, testVersion)
+			hcs.UpdateVersion(testName, testVersion)
 
-	if len(hcs.Versions) != 1 {
-		t.Error("Should be able to add a new version to an empty version array")
-	}
+			It("Versions array should be with one element", func() {
+				Expect(len(hcs.Versions)).Should(Equal(1))
+			})
 
-	if hcs.Versions[0].Name != testName {
-		t.Errorf(`Version name should be "%s" but it's "%s"`, testName, hcs.Versions[0].Name)
-	}
+			It(`The version name should be "aName"`, func() {
+				Expect(hcs.Versions[0].Name).Should(Equal(testName))
+			})
 
-	if hcs.Versions[0].Version != testVersion {
-		t.Errorf(`Version should be "%s" but it's "%s"`, testVersion, hcs.Versions[0].Version)
-	}
-}
+			It(`The version should be "aVersion"`, func() {
+				Expect(hcs.Versions[0].Version).Should(Equal(testVersion))
+			})
+		})
 
-func TestHyperConvergedStatus_UpdateVersion_addNew(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: "aaa", Version: "1.2.3"},
-			{Name: "bbb", Version: "4.5.6"},
-			{Name: "ccc", Version: "7.8.9"},
-		},
-	}
+		Context("Should be able to add a new version to an empty version array", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{},
+			}
 
-	hcs.UpdateVersion(testName, testVersion)
+			hcs.UpdateVersion(testName, testVersion)
 
-	if len(hcs.Versions) != 4 {
-		t.Errorf("Should be able to add a new version to a non-empty version array")
-	}
+			It("Versions array should be with one element", func() {
+				Expect(len(hcs.Versions)).Should(Equal(1))
+			})
 
-	if hcs.Versions[3].Name != testName {
-		t.Errorf(`Version name should be ""%s"" but it's "%s"`, testName, hcs.Versions[3].Name)
-	}
+			It(`The version name should be "aName"`, func() {
+				Expect(hcs.Versions[0].Name).Should(Equal(testName))
+			})
 
-	if hcs.Versions[3].Version != testVersion {
-		t.Errorf(`Version should be ""%s"" but it's "%s"`, testVersion, hcs.Versions[3].Version)
-	}
-}
+			It(`The version should be "aVersion"`, func() {
+				Expect(hcs.Versions[0].Version).Should(Equal(testVersion))
+			})
+		})
 
-func TestHyperConvergedStatus_UpdateVersion_updateFirst(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: testName, Version: testOldVersion},
-			{Name: "bbb", Version: "4.5.6"},
-			{Name: "ccc", Version: "7.8.9"},
-		},
-	}
+		Context("Should be able to add a new version to an existing version array", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: "aaa", Version: "1.2.3"},
+					{Name: "bbb", Version: "4.5.6"},
+					{Name: "ccc", Version: "7.8.9"},
+				},
+			}
 
-	hcs.UpdateVersion(testName, testVersion)
+			hcs.UpdateVersion(testName, testVersion)
 
-	if len(hcs.Versions) != 3 {
-		t.Errorf("Should be able to update an existing version; array length should be 3, but it's %d", len(hcs.Versions))
-	}
+			It("Versions array should be with four elements", func() {
+				Expect(len(hcs.Versions)).Should(Equal(4))
+			})
 
-	if hcs.Versions[0].Name != testName {
-		t.Errorf(`Version name should be "%s" but it's "%s"`, testName, hcs.Versions[0].Name)
-	}
+			It(`The version name should be "aName"`, func() {
+				Expect(hcs.Versions[3].Name).Should(Equal(testName))
+			})
 
-	if hcs.Versions[0].Version != testVersion {
-		t.Errorf(`Version should be "%s" but it's "%s"`, testVersion, hcs.Versions[0].Version)
-	}
-}
+			It(`The version should be "aVersion"`, func() {
+				Expect(hcs.Versions[3].Version).Should(Equal(testVersion))
+			})
+		})
 
-func TestHyperConvergedStatus_UpdateVersion_updateMiddle(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: "aaa", Version: "1.2.3"},
-			{Name: testName, Version: testOldVersion},
-			{Name: "ccc", Version: "7.8.9"},
-		},
-	}
+		Context("Should be able to update a new version in an existing version array (first element)", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: testName, Version: testOldVersion},
+					{Name: "bbb", Version: "4.5.6"},
+					{Name: "ccc", Version: "7.8.9"},
+				},
+			}
 
-	hcs.UpdateVersion(testName, testVersion)
+			hcs.UpdateVersion(testName, testVersion)
 
-	if len(hcs.Versions) != 3 {
-		t.Errorf("Should be able to update an existing version; array length should be 3, but it's %d", len(hcs.Versions))
-	}
+			It("Versions array should be with three elements", func() {
+				Expect(len(hcs.Versions)).Should(Equal(3))
+			})
 
-	if hcs.Versions[1].Name != testName {
-		t.Errorf(`Version name should be "%s" but it's "%s"`, testName, hcs.Versions[1].Name)
-	}
+			It(`The version name should be "aName"`, func() {
+				Expect(hcs.Versions[0].Name).Should(Equal(testName))
+			})
 
-	if hcs.Versions[1].Version != testVersion {
-		t.Errorf(`Version should be "%s" but it's "%s"`, testVersion, hcs.Versions[1].Version)
-	}
-}
+			It(`The version should be "aVersion"`, func() {
+				Expect(hcs.Versions[0].Version).Should(Equal(testVersion))
+			})
+		})
 
-func TestHyperConvergedStatus_UpdateVersion_updateLast(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: "aaa", Version: "1.2.3"},
-			{Name: "bbb", Version: "4.5.6"},
-			{Name: testName, Version: testOldVersion},
-		},
-	}
+		Context("Should be able to update a new version in an existing version array (middle element)", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: "aaa", Version: "1.2.3"},
+					{Name: testName, Version: testOldVersion},
+					{Name: "ccc", Version: "7.8.9"},
+				},
+			}
 
-	hcs.UpdateVersion(testName, testVersion)
+			hcs.UpdateVersion(testName, testVersion)
 
-	if len(hcs.Versions) != 3 {
-		t.Errorf("Should be able to update an existing version; array length should be 3, but it's %d", len(hcs.Versions))
-	}
+			It("Versions array should be with three elements", func() {
+				Expect(len(hcs.Versions)).Should(Equal(3))
+			})
 
-	if hcs.Versions[2].Name != testName {
-		t.Errorf(`Version name should be "%s" but it's "%s"`, testName, hcs.Versions[2].Name)
-	}
+			It(`The version name should be "aName"`, func() {
+				Expect(hcs.Versions[1].Name).Should(Equal(testName))
+			})
 
-	if hcs.Versions[2].Version != testVersion {
-		t.Errorf(`Version should be "%s" but it's "%s"`, testVersion, hcs.Versions[2].Version)
-	}
-}
+			It(`The version should be "aVersion"`, func() {
+				Expect(hcs.Versions[1].Version).Should(Equal(testVersion))
+			})
+		})
 
-func TestHyperConvergedStatus_GetVersion_nil(t *testing.T) {
-	hcs := &HyperConvergedStatus{Conditions: []conditionsv1.Condition{}, RelatedObjects: []corev1.ObjectReference{}}
-	ver, ok := hcs.GetVersion(testName)
-	if ok || ver != "" {
-		t.Error("Should not find the version in empty version array")
-	}
-}
+		Context("Should be able to update a new version in an existing version array (last element)", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: "aaa", Version: "1.2.3"},
+					{Name: "bbb", Version: "4.5.6"},
+					{Name: testName, Version: testOldVersion},
+				},
+			}
 
-func TestHyperConvergedStatus_GetVersion_empty(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions:       Versions{},
-	}
-	ver, ok := hcs.GetVersion(testName)
-	if ok || ver != "" {
-		t.Error("Should not find the version in empty version array")
-	}
-}
+			hcs.UpdateVersion(testName, testVersion)
 
-func TestHyperConvergedStatus_GetVersion_notFound(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: "aaa", Version: "1.2.3"},
-			{Name: "bbb", Version: "4.5.6"},
-			{Name: "ccc", Version: "7.8.9"},
-		},
-	}
-	ver, ok := hcs.GetVersion(testName)
-	if ok || ver != "" {
-		t.Error("Should not find the version; it should be missing")
-	}
-}
+			It("Versions array should be with three elements", func() {
+				Expect(len(hcs.Versions)).Should(Equal(3))
+			})
 
-func TestHyperConvergedStatus_GetVersion_findFirst(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: testName, Version: testVersion},
-			{Name: "bbb", Version: "4.5.6"},
-			{Name: "ccc", Version: "7.8.9"},
-		},
-	}
-	ver, ok := hcs.GetVersion(testName)
-	if !ok {
-		t.Errorf(`version "%s"" should be found`, testName)
-	}
-	if ver != testVersion {
-		t.Errorf(`version "%s"" should be "%s"; but it's %s`, testName, testVersion, ver)
-	}
-}
+			It(`The version name should be "aName"`, func() {
+				Expect(hcs.Versions[2].Name).Should(Equal(testName))
+			})
 
-func TestHyperConvergedStatus_GetVersion_findMiddle(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: "aaa", Version: "1.2.3"},
-			{Name: testName, Version: testVersion},
-			{Name: "ccc", Version: "7.8.9"},
-		},
-	}
-	ver, ok := hcs.GetVersion(testName)
-	if !ok {
-		t.Errorf(`version "%s"" should be found`, testName)
-	}
-	if ver != testVersion {
-		t.Errorf(`version "%s"" should be "%s"; but it's %s`, testName, testVersion, ver)
-	}
-}
+			It(`The version should be "aVersion"`, func() {
+				Expect(hcs.Versions[2].Version).Should(Equal(testVersion))
+			})
+		})
 
-func TestHyperConvergedStatus_GetVersion_findLast(t *testing.T) {
-	hcs := &HyperConvergedStatus{
-		Conditions:     []conditionsv1.Condition{},
-		RelatedObjects: []corev1.ObjectReference{},
-		Versions: Versions{
-			{Name: "aaa", Version: "1.2.3"},
-			{Name: "bbb", Version: "4.5.6"},
-			{Name: testName, Version: testVersion},
-		},
-	}
-	ver, ok := hcs.GetVersion(testName)
-	if !ok {
-		t.Errorf(`version "%s"" should be found`, testName)
-	}
-	if ver != testVersion {
-		t.Errorf(`version "%s"" should be "%s"; but it's %s`, testName, testVersion, ver)
-	}
-}
+	})
+
+	Describe("HyperConvergedStatus.GetVersion", func() {
+		Context("should return empty response for nil array", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+			}
+
+			ver, ok := hcs.GetVersion(testName)
+
+			It("should not find the version", func() {
+				Expect(ok).To(BeFalse())
+			})
+			It("the version should be empty", func() {
+				Expect(ver).To(BeEmpty())
+			})
+		})
+
+		Context("should return empty response for empty array", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{},
+			}
+
+			ver, ok := hcs.GetVersion(testName)
+
+			It("should not find the version", func() {
+				Expect(ok).To(BeFalse())
+			})
+
+			It("the version should be empty", func() {
+				Expect(ver).To(BeEmpty())
+			})
+		})
+
+		Context("should return empty response if the version is not in the versions array", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: "aaa", Version: "1.2.3"},
+					{Name: "bbb", Version: "4.5.6"},
+					{Name: "ccc", Version: "7.8.9"},
+				},
+			}
+
+			ver, ok := hcs.GetVersion(testName)
+
+			It("should not find the version", func() {
+				Expect(ok).To(BeFalse())
+			})
+
+			It("the version should be empty", func() {
+				Expect(ver).To(BeEmpty())
+			})
+		})
+
+		Context("should return a valid response if the version is in the versions array (first element)", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: testName, Version: testVersion},
+					{Name: "bbb", Version: "4.5.6"},
+					{Name: "ccc", Version: "7.8.9"},
+				},
+			}
+
+			ver, ok := hcs.GetVersion(testName)
+
+			It("should not find the version", func() {
+				Expect(ok).To(BeTrue())
+			})
+
+			It("the version should be empty", func() {
+				Expect(ver).Should(Equal(testVersion))
+			})
+		})
+
+		Context("should return a valid response if the version is in the versions array (middle element)", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: "aaa", Version: "1.2.3"},
+					{Name: testName, Version: testVersion},
+					{Name: "ccc", Version: "7.8.9"},
+				},
+			}
+
+			ver, ok := hcs.GetVersion(testName)
+
+			It("should not find the version", func() {
+				Expect(ok).To(BeTrue())
+			})
+
+			It("the version should be empty", func() {
+				Expect(ver).Should(Equal(testVersion))
+			})
+		})
+
+		Context("should return a valid response if the version is in the versions array (last element)", func() {
+			hcs := &HyperConvergedStatus{
+				Conditions:     []conditionsv1.Condition{},
+				RelatedObjects: []corev1.ObjectReference{},
+				Versions:       Versions{
+					{Name: "aaa", Version: "1.2.3"},
+					{Name: "bbb", Version: "4.5.6"},
+					{Name: testName, Version: testVersion},
+				},
+			}
+
+			ver, ok := hcs.GetVersion(testName)
+
+			It("should not find the version", func() {
+				Expect(ok).To(BeTrue())
+			})
+
+			It("the version should be empty", func() {
+				Expect(ver).Should(Equal(testVersion))
+			})
+		})
+	})
+})
