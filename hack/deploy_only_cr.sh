74bdf0b5219b2bffdbe3b30b5187be680daae509
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2020-09-13 18:13:53 +0300

    index-image CI: support KVM_EMULATION test configuration
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- hack/deploy_only_cr.sh
+++ hack/deploy_only_cr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 HCO_NAMESPACE="kubevirt-hyperconverged"
 HCO_KIND="hyperconvergeds"
@@ -17,16 +17,35 @@ fi
 function cleanup() {
     rv=$?
     if [ "x$rv" != "x0" ]; then
-        echo "Error during upgrade: exit status: $rv"
+        echo "Error during HCO CR deployment: exit status: $rv"
         make dump-state
-        echo "*** Upgrade test failed ***"
+        echo "*** HCO CR deployment failed ***"
     fi
     exit $rv
 }
 
 trap "cleanup" INT TERM EXIT
 
+if [[ -n "${KVM_EMULATION}" ]]; then
+  SUBSCRIPTION_NAME=$(oc get subscription -n "${HCO_NAMESPACE}" -o name)
+  # cut the type prefix, e.g. subscription.operators.coreos.com/kubevirt-hyperconverged => kubevirt-hyperconverged
+  SUBSCRIPTION_NAME=${SUBSCRIPTION_NAME/*\//}
+
+  TMP_DIR=$(mktemp -d)
+  cat > "${TMP_DIR}/subscription-patch.yaml" << EOF
+spec:
+  config:
+    selector:
+      matchLabels:
+        name: hyperconverged-cluster-operator
+    env:
+    - name: 'KVM_EMULATION'
+      value: "${KVM_EMULATION}"
+EOF
+
+  ${CMD} patch -n "${HCO_NAMESPACE}" Subscription "${SUBSCRIPTION_NAME}" --patch="$(cat "${TMP_DIR}/subscription-patch.yaml")" --type=merge
+fi
+
 ${CMD} apply -n kubevirt-hyperconverged -f deploy/hco.cr.yaml
 
 ${CMD} wait -n "${HCO_NAMESPACE}" "${HCO_KIND}" "${HCO_RESOURCE_NAME}" --for condition=Available --timeout="30m"
-${CMD} wait deployment "${HCO_DEPLOYMENT_NAME}" --for condition=Available -n "${HCO_NAMESPACE}" --timeout="30m"
