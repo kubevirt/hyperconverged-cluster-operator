f560e2207e2e1414aca66b83619d686bc2eb6e80
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2020-09-21 15:20:53 +0300

    fix oc get command
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- hack/upgrade-test.sh
+++ hack/upgrade-test.sh
@@ -227,7 +227,7 @@ ${CMD} wait deployment ${HCO_DEPLOYMENT_NAME} --for condition=Available -n ${HCO
 Msg "verify the hyperconverged-cluster deployment is using the new image"
 
 SEARCH_PHRASE="${OPENSHIFT_BUILD_NAMESPACE}/stable"
-./hack/retry.sh 6 30 "${CMD} get -n ${HCO_NAMESPACE} deployment ${HCO_DEPLOYMENT_NAME} jsonpath=\"{ .spec.template.spec.containers[0].image }\" | grep ${SEARCH_PHRASE}"
+./hack/retry.sh 6 30 "${CMD} get -n ${HCO_NAMESPACE} deployment ${HCO_DEPLOYMENT_NAME} -o jsonpath=\"{ .spec.template.spec.containers[0].image }\" | grep ${SEARCH_PHRASE}"
 
 Msg "wait that cluster is operational after upgrade"
 timeout 20m bash -c 'export CMD="${CMD}";exec ./hack/check-state.sh'
