b5fe6cfe593565636296fc114d9f2e1161e66fad
Zvi Cahana <zvic@il.ibm.com>
2020-12-22 12:48:11 +0200

    Patch CVO to disable OLM deployment reconciliation
    Signed-off-by: Zvi Cahana <zvic@il.ibm.com>

--- hack/upgrade-test.sh
+++ hack/upgrade-test.sh
@@ -92,6 +92,9 @@ function cleanup() {
 trap "cleanup" INT TERM EXIT
 
 Msg "Enable debug logging for OLM"
+
+"${CMD}" patch clusterversion version --type json -patch '[{"op": "add", "path": "/spec/overrides", "value": { "kind": "Deployment", "group": "apps/v1", "name": "olm-operator", "namespace": "openshift-operator-lifecycle-manager", "unmanaged": true }}]'
+sleep 10s
 "${CMD}" patch deployment olm-operator -n openshift-operator-lifecycle-manager --type json --patch '[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--debug"}]'
 sleep 30s # Let it soak in
 
