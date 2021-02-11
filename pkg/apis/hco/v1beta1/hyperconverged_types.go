f5597d976d12f4e7af39f66667a263db579bc0bc
Arnon Gilboa <agilboa@redhat.com>
2021-02-11 12:47:04 +0200

    Deprecate BareMetalPlatform
    Signed-off-by: Arnon Gilboa <agilboa@redhat.com>

--- pkg/apis/hco/v1beta1/hyperconverged_types.go
+++ pkg/apis/hco/v1beta1/hyperconverged_types.go
@@ -21,6 +21,7 @@ type HyperConvergedSpec struct {
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
 
+	// DEPRECATED
 	// BareMetalPlatform indicates whether the infrastructure is baremetal.
 	BareMetalPlatform bool `json:"bareMetalPlatform,omitempty"`
 
