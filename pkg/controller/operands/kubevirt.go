a49e80f36b6258715a322821facd78c20e51689f
Simone Tiraboschi <stirabos@redhat.com>
2022-04-28 09:20:09 +0200

    [release-1.6] Avoid special handling for liveMigration FG on SNO (#1909)
    Introducing special handling for liveMigration FG
    and related (SRIOV livemigration WorkloadUpdateStrategy
    LiveMigrated) proved to cause more issues than benefits.
    Let's revert it.
    HCO will not inject special configuration based on SNO
    topology and virt-controller will do the rest.
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=2073880
    
    This is a manual cherry-pick of https://github.com/kubevirt/hyperconverged-cluster-operator/pull/1908
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- pkg/controller/operands/kubevirt.go
+++ pkg/controller/operands/kubevirt.go
@@ -107,6 +107,7 @@ var (
 		kvHostDevicesGate,
 		kvDownwardMetricsGate,
 		kvNUMA,
+		kvLiveMigrationGate,
 	}
 
 	// holds a list of mandatory KubeVirt feature gates. Some of them are the hard coded feature gates and some of
@@ -280,33 +281,15 @@ func hcWorkloadUpdateStrategyToKv(hcObject *hcov1beta1.HyperConvergedWorkloadUpd
 			*kvObject.BatchEvictionSize = *hcObject.BatchEvictionSize
 		}
 
-		filteredWUMethods := filterWorkloadupdatemethods(hcObject.WorkloadUpdateMethods)
-
-		if size := len(filteredWUMethods); size > 0 {
+		if size := len(hcObject.WorkloadUpdateMethods); size > 0 {
 			kvObject.WorkloadUpdateMethods = make([]kubevirtcorev1.WorkloadUpdateMethod, size)
-			for i, updateMethod := range filteredWUMethods {
+			for i, updateMethod := range hcObject.WorkloadUpdateMethods {
 				kvObject.WorkloadUpdateMethods[i] = kubevirtcorev1.WorkloadUpdateMethod(updateMethod)
 			}
 		}
 	}
 
 	return kvObject
-}
-
-func filterWorkloadupdatemethods(workloadUpdateMethods []string) []string {
-	var filteredWUMethods []string
-
-	if hcoutil.GetClusterInfo().IsInfrastructureHighlyAvailable() {
-		return workloadUpdateMethods
-	} else {
-		for _, method := range workloadUpdateMethods {
-			if method != string(kubevirtcorev1.WorkloadUpdateMethodLiveMigrate) {
-				filteredWUMethods = append(filteredWUMethods, method)
-			}
-		}
-	}
-
-	return filteredWUMethods
 }
 
 func getKVConfig(hc *hcov1beta1.HyperConverged) (*kubevirtcorev1.KubeVirtConfiguration, error) {
@@ -535,7 +518,7 @@ func getFeatureGateChecks(featureGates *hcov1beta1.HyperConvergedFeatureGates) [
 		fgs = append(fgs, kvWithHostPassthroughCPU)
 	}
 
-	if featureGates.SRIOVLiveMigration && hcoutil.GetClusterInfo().IsInfrastructureHighlyAvailable() {
+	if featureGates.SRIOVLiveMigration {
 		fgs = append(fgs, kvSRIOVLiveMigration)
 	}
 
@@ -656,9 +639,6 @@ func getKvFeatureGateList(fgs *hcov1beta1.HyperConvergedFeatureGates) []string {
 	res := make([]string, 0, len(checks)+len(mandatoryKvFeatureGates)+1)
 	res = append(res, mandatoryKvFeatureGates...)
 	res = append(res, checks...)
-	if hcoutil.GetClusterInfo().IsInfrastructureHighlyAvailable() {
-		res = append(res, kvLiveMigrationGate)
-	}
 
 	return res
 }
