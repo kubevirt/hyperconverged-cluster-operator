650be91ea86311b1ed18c6fbb96abbb036158433
Simone Tiraboschi <stirabos@redhat.com>
2022-06-15 06:30:02 +0200

    [release-1.6] Expose a knob to let the user tune default CPU model (#2004) (#2006)
    Expose a knob to let the user tune the cluster-wide
    default CPU model.
    
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=2096287
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- pkg/apis/hco/v1beta1/zz_generated.openapi.go
+++ pkg/apis/hco/v1beta1/zz_generated.openapi.go
@@ -341,6 +341,13 @@ func schema_pkg_apis_hco_v1beta1_HyperConvergedSpec(ref common.ReferenceCallback
 							Format:      "",
 						},
 					},
+					"defaulCPUModel": {
+						SchemaProps: spec.SchemaProps{
+							Description: "DefaultCPUModel defines a cluster default for CPU model: default CPU model is set when vmi doesn't have any cpu model. When vmi has cpu model set, then vmi's cpu model is preferred. When default cpu model is not set and vmi's cpu model is not set too, host-model will be set. Default cpu model can be changed when kubevirt is running.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
 					"obsoleteCPUs": {
 						SchemaProps: spec.SchemaProps{
 							Description: "ObsoleteCPUs allows avoiding scheduling of VMs for obsolete CPU models",
