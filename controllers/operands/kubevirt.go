acb1d777b445229a0ef55b91a17ba9867318b266
Alex Kalenyuk <akalenyu@redhat.com>
2022-08-22 17:17:50 +0300

    Enable VMExport feature gate by default
    Allows exporting VMs to the outside of the cluster.
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- controllers/operands/kubevirt.go
+++ controllers/operands/kubevirt.go
@@ -88,6 +88,9 @@ const (
 
 	// Allow automatic numa mapping on VMs with dedicated CPUs, if requested
 	kvNUMA = "NUMA"
+
+	// Export VMs to outside of the cluster
+	kvVMExportGate = "VMExport"
 )
 
 var (
@@ -103,6 +106,8 @@ var (
 		kvHostDevicesGate,
 		kvDownwardMetricsGate,
 		kvNUMA,
+		kvLiveMigrationGate,
+		kvVMExportGate,
 	}
 
 	// holds a list of mandatory KubeVirt feature gates. Some of them are the hard coded feature gates and some of
