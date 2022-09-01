e290a897d96e3ddc32243efe11ddfd6f519529fb
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2022-09-01 12:19:42 +0300

    [clean code]: fix the receiver in methods of OperandHandler and ReconcileHyperConverged
    A type method should be either all with pointer receiver or with a regular receiver,
    but not both.
    
    This commit set all the methods in the OperandHandler and ReconcileHyperConverged types
    to be pointers.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- controllers/operands/operandHandler.go
+++ controllers/operands/operandHandler.go
@@ -74,6 +74,7 @@ func NewOperandHandler(client client.Client, scheme *runtime.Scheme, ci hcoutil.
 	}
 }
 
+// FirstUseInitiation is a lazy init function
 // The k8s client is not available when calling to NewOperandHandler.
 // Initial operations that need to read/write from the cluster can only be done when the client is already working.
 func (h *OperandHandler) FirstUseInitiation(scheme *runtime.Scheme, ci hcoutil.ClusterInfo, hc *hcov1beta1.HyperConverged) {
@@ -130,7 +131,7 @@ func (h *OperandHandler) addOperands(scheme *runtime.Scheme, hc *hcov1beta1.Hype
 	}
 }
 
-func (h OperandHandler) Ensure(req *common.HcoRequest) error {
+func (h *OperandHandler) Ensure(req *common.HcoRequest) error {
 	for _, handler := range h.operands {
 		res := handler.ensure(req)
 		if res.Err != nil {
@@ -161,7 +162,7 @@ func (h OperandHandler) Ensure(req *common.HcoRequest) error {
 
 }
 
-func (h OperandHandler) handleUpdatedOperand(req *common.HcoRequest, res *EnsureResult) {
+func (h *OperandHandler) handleUpdatedOperand(req *common.HcoRequest, res *EnsureResult) {
 	if !res.Overwritten {
 		h.eventEmitter.EmitEvent(req.Instance, corev1.EventTypeNormal, "Updated", fmt.Sprintf("Updated %s %s", res.Type, res.Name))
 	} else {
@@ -175,7 +176,7 @@ func (h OperandHandler) handleUpdatedOperand(req *common.HcoRequest, res *Ensure
 	}
 }
 
-func (h OperandHandler) EnsureDeleted(req *common.HcoRequest) error {
+func (h *OperandHandler) EnsureDeleted(req *common.HcoRequest) error {
 
 	tCtx, cancel := context.WithTimeout(req.Ctx, deleteTimeOut)
 	defer cancel()
