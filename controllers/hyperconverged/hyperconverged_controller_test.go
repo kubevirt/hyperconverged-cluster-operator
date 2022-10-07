4492325a0e661fad063199e0676347836dc37b76
Simone Tiraboschi <stirabos@redhat.com>
2022-10-07 18:12:03 +0200

    [release-1.7] Enable PSA FG on Kubevirt (#2104)
    Enable PSA FG on Kubevirt to be compatible with
    k8s >= 1.24 and derivates with PSA in enforcing mode.
    
    Once available also in older versions of Kubevirt,
    we will have also to backport it to release-1.6
    and release-1.7.
    
    This is a manual cherry-pick of #2093
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- controllers/hyperconverged/hyperconverged_controller_test.go
+++ controllers/hyperconverged/hyperconverged_controller_test.go
@@ -209,6 +209,7 @@ var _ = Describe("HyperconvergedController", func() {
 					"ExpandDisks",
 					"NUMA",
 					"WithHostPassthroughCPU",
+					"PSA",
 				}
 				// Get the KV
 				kvList := &kubevirtcorev1.KubeVirtList{}
