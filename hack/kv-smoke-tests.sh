66c6c46177079b2bd09bfdd8b7bd4589ff74d3e2
Simone Tiraboschi <stirabos@redhat.com>
2022-11-15 13:20:27 +0100

    Revert "[release-1.8] Enable PSA FG on Kubevirt (#2093)" (#2135)
    * Revert "Enable PSA FG on Kubevirt (#2093)"
    
    This reverts commit 4731026dec411bb0a0f5ce3153f41ff948390a34.
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>
    
    * hack: brutal workaround to bypass SCC->PSA on OCP
    
    brutal workaround to bypass SCC -> PSA on OCP >= 4.12
    
    remove ASAP!!!
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- hack/kv-smoke-tests.sh
+++ hack/kv-smoke-tests.sh
@@ -23,6 +23,10 @@ curl -Lo "$TESTS_BINARY" "https://storage.googleapis.com/kubevirt-prow/devel/nig
 ######
 chmod +x "$TESTS_BINARY"
 
+# TODO: brutal workaround to bypass SCC -> PSA on OCP >= 4.12, remove ASAP!!!
+${CMD} adm policy add-scc-to-group privileged system:authenticated
+######
+
 echo "create testing infrastructure"
 
 cat <<EOF | ${CMD} apply -f -
