c7ec2465e4aac742ef860ac07f281adc96a666fd
Simone Tiraboschi <stirabos@redhat.com>
2022-12-13 14:53:34 +0100

    [release-1.8] Avoid omitempty on WorkloadUpdateMethods (#2180)
    In our API for WorkloadUpdateMethods we read:
    ...
    An empty list defaults to no automated workload updating.
    
    and a default of: ["LiveMigrate"]
    
    but in the past the json representation of
    WorkloadUpdateMethods was defined with omitempty
    but, as for:
    https://go.dev/src/encoding/json/encode.go
    // The "omitempty" option specifies that the field should be omitted
    // from the encoding if the field has an empty value, defined as
    // false, 0, a nil pointer, a nil interface value, and any empty array,
    // slice, map, or string.
    
    the golang json encoder is omitting also empty array and slice
    and so an empty list on WorkloadUpdateMethods gets omitted at json
    level and so the APIServer will then replace it with the
    default value (["LiveMigrate"]).
    
    Avoid declaring omitempty on WorkloadUpdateMethods to
    make an empty list an acceptable value.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2151693
    
    This is a manual cherry-pick of https://github.com/kubevirt/hyperconverged-cluster-operator/pull/2180
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- api/v1beta1/hyperconverged_types.go
+++ api/v1beta1/hyperconverged_types.go
@@ -389,8 +389,7 @@ type HyperConvergedWorkloadUpdateStrategy struct {
 	//
 	// +listType=atomic
 	// +kubebuilder:default={"LiveMigrate"}
-	// +optional
-	WorkloadUpdateMethods []string `json:"workloadUpdateMethods,omitempty"`
+	WorkloadUpdateMethods []string `json:"workloadUpdateMethods"`
 
 	// BatchEvictionSize Represents the number of VMIs that can be forced updated per
 	// the BatchShutdownInterval interval
