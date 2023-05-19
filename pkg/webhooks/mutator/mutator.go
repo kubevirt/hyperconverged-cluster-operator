b5518da634edd7e9e5bb928095b471e80f220d66
Simone Tiraboschi <stirabos@redhat.com>
2023-05-19 06:36:43 +0200

    Revert "[release-1.8]: Kubevirt: Enforce limits & requests by a configurable ratio (#2280)" (#2346)
    This reverts commit 6b2a0e443452704ad1379e9bec27d50556f4e405.
    This reverts PR #2280
    
    Signed-off-by: stirabos <stirabos@redhat.com>

--- pkg/webhooks/mutator/mutator.go
+++ pkg/webhooks/mutator/mutator.go
@@ -7,19 +7,23 @@ import (
 	admissionv1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+
+	"github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
+	hcoutil "github.com/kubevirt/hyperconverged-cluster-operator/pkg/util"
 )
 
 const (
 	ignoreOperationMessage   = "ignoring other operations"
-	admittingDeletionMessage = "the hcoNamespace doesn't contain HyperConverged CR, admitting its deletion"
-	deniedDeletionMessage    = "HyperConverged CR is still present, please remove it before deleting the containing hcoNamespace"
+	admittingDeletionMessage = "the namespace doesn't contain HyperConverged CR, admitting its deletion"
+	deniedDeletionMessage    = "HyperConverged CR is still present, please remove it before deleting the containing namespace"
 )
 
 var (
-	logger = logf.Log.WithName("hcoNamespace mutator")
+	logger = logf.Log.WithName("mutator")
 
 	_ admission.Handler = &NsMutator{}
 )
@@ -38,11 +42,11 @@ func NewNsMutator(cli client.Client, namespace string) *NsMutator {
 	}
 }
 
-func (nm *NsMutator) Handle(ctx context.Context, req admission.Request) admission.Response {
+func (nm *NsMutator) Handle(_ context.Context, req admission.Request) admission.Response {
 	logger.Info("reaching NsMutator.Handle")
 
 	if req.Operation == admissionv1.Delete {
-		return nm.handleNsDelete(ctx, req)
+		return nm.handleNsDelete(req)
 	}
 
 	// ignoring other operations
@@ -50,18 +54,18 @@ func (nm *NsMutator) Handle(ctx context.Context, req admission.Request) admissio
 
 }
 
-func (nm *NsMutator) handleNsDelete(ctx context.Context, req admission.Request) admission.Response {
+func (nm *NsMutator) handleNsDelete(req admission.Request) admission.Response {
 	ns := &corev1.Namespace{}
 
 	// In reference to PR: https://github.com/kubernetes/kubernetes/pull/76346
 	// OldObject contains the object being deleted
 	err := nm.decoder.DecodeRaw(req.OldObject, ns)
 	if err != nil {
-		logger.Error(err, "failed decoding hcoNamespace object")
+		logger.Error(err, "failed decoding namespace object")
 		return admission.Errored(http.StatusBadRequest, err)
 	}
 
-	admitted, err := nm.handleMutatingNsDelete(ctx, ns)
+	admitted, err := nm.handleMutatingNsDelete(ns)
 	if err != nil {
 		return admission.Errored(http.StatusInternalServerError, err)
 	}
@@ -82,23 +86,35 @@ func (nm *NsMutator) InjectDecoder(d *admission.Decoder) error {
 	return nil
 }
 
-func (nm *NsMutator) handleMutatingNsDelete(ctx context.Context, ns *corev1.Namespace) (bool, error) {
-	logger.Info("validating hcoNamespace deletion", "name", ns.Name)
+func (nm *NsMutator) handleMutatingNsDelete(ns *corev1.Namespace) (bool, error) {
+	logger.Info("validating namespace deletion", "name", ns.Name)
 
 	if ns.Name != nm.namespace {
-		logger.Info("ignoring request for a different hcoNamespace")
+		logger.Info("ignoring request for a different namespace")
 		return true, nil
 	}
 
-	// Block the deletion if the hcoNamespace with a clear error message
+	ctx := context.TODO()
+	hco := &v1beta1.HyperConverged{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      hcoutil.HyperConvergedName,
+			Namespace: nm.namespace,
+		},
+	}
+
+	// Block the deletion if the namespace with a clear error message
 	// if HCO CR is still there
-	_, err := getHcoObject(ctx, nm.cli, nm.namespace)
+
+	found := &v1beta1.HyperConverged{}
+	err := nm.cli.Get(ctx, client.ObjectKeyFromObject(hco), found)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
+			logger.Info("HCO CR doesn't not exist, allow namespace deletion")
 			return true, nil
 		}
+		logger.Error(err, "failed getting HyperConverged CR")
 		return false, err
 	}
-	logger.Info("HCO CR still exists, forbid hcoNamespace deletion")
+	logger.Info("HCO CR still exists, forbid namespace deletion")
 	return false, nil
 }
