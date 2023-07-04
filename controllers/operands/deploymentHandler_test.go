kubevirt/kubevirt

Adds labels to kubevirt-console-plugin pod (#2327)
* Adds labels to kubevirt-console-plugin pod

Signed-off-by: Dharmit Shah <shahdharmit@gmail.com>

* Recreate deployment if LabelSelector changes during upgrade

Signed-off-by: Dharmit Shah <shahdharmit@gmail.com>

* Changes based on Simone's feedback

Signed-off-by: Dharmit Shah <shahdharmit@gmail.com>

* Various changes

Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

---------

Signed-off-by: Dharmit Shah <shahdharmit@gmail.com>
Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

controllers/operands/deploymentHandler_test.go
@@ -1,0 +1,121 @@
+package operands
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	hcov1beta1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
+	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/common"
+	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/commontestutils"
+)
+
+var _ = Describe("Deployment Handler", func() {
+	Context("update or recreate the Deployment as required", func() {
+		var hco *hcov1beta1.HyperConverged
+		var req *common.HcoRequest
+		var expectedDeployment *appsv1.Deployment
+
+		BeforeEach(func() {
+			hco = commontestutils.NewHco()
+			req = commontestutils.NewReq(hco)
+
+			expectedDeployment = &appsv1.Deployment{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Deployment",
+					APIVersion: "apps/v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "modifiedDeployment",
+					Labels: map[string]string{"key1": "value1"},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"key1": "value1"},
+					},
+				},
+			}
+		})
+
+		It("should recreate the Deployment as LabelSelector has changed", func() {
+			modifiedDeployment := &appsv1.Deployment{}
+			expectedDeployment.DeepCopyInto(modifiedDeployment)
+			// modify the LabelSelector
+			modifiedDeployment.Spec.Selector = &metav1.LabelSelector{
+				MatchLabels: map[string]string{"key2": "value2"},
+			}
+			modifiedDeployment.ObjectMeta.UID = "oldObjectUID"
+
+			// let's initialize the fake client with a modified object
+			cl := commontestutils.InitClient([]client.Object{modifiedDeployment})
+
+			foundResource := &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+
+			// let's ensure the handler properly reconcile it back to the expected state
+			handler := newDeploymentHandler(cl, commontestutils.GetScheme(), expectedDeployment)
+			res := handler.ensure(req)
+			Expect(res.Updated).To(BeTrue())
+			Expect(res.Err).ToNot(HaveOccurred())
+
+			foundResource = &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+
+			Expect(foundResource.Spec.Selector).Should(Equal(expectedDeployment.Spec.Selector))
+			// let's check the object UID to ensure that the object get really deleted and recreated
+			Expect(foundResource.ObjectMeta.UID).ToNot(Equal(modifiedDeployment.ObjectMeta.UID))
+		})
+
+		It("should only update, not recreate, the Deployment since LabelSelector hasn't changed", func() {
+			modifiedDeployment := &appsv1.Deployment{}
+			expectedDeployment.DeepCopyInto(modifiedDeployment)
+			// modify only the labels
+			gotLabels := modifiedDeployment.GetLabels()
+			gotLabels["key2"] = "value2"
+			modifiedDeployment.SetLabels(gotLabels)
+			modifiedDeployment.ObjectMeta.UID = "oldObjectUID"
+
+			// let's initialize the fake client with a modified object
+			cl := commontestutils.InitClient([]client.Object{modifiedDeployment})
+			foundResource := &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+
+			// let's ensure the handler properly reconcile it back to the expected state
+			handler := newDeploymentHandler(cl, commontestutils.GetScheme(), expectedDeployment)
+			res := handler.ensure(req)
+			Expect(res.Updated).To(BeTrue())
+			Expect(res.Err).ToNot(HaveOccurred())
+
+			foundResource = &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+
+			Expect(foundResource.Spec.Selector).Should(Equal(expectedDeployment.Spec.Selector))
+			// let's check the object UID to ensure that the object get updated and not deleted and recreated
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+		})
+	})
+
+})

controllers/operands/kubevirtConsolePlugin_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"reflect"
 
+	"k8s.io/utils/pointer"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	consolev1 "github.com/openshift/api/console/v1"
@@ -221,6 +223,11 @@ var _ = Describe("Kubevirt Console Plugin", func() {
 			).ToNot(HaveOccurred())
 			Expect(foundResource.Name).To(Equal(expectedResource.Name))
 			Expect(foundResource.Labels).Should(HaveKeyWithValue(hcoutil.AppLabel, commontestutils.Name))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabel, commontestutils.Name))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelComponent, string(hcoutil.AppComponentDeployment)))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelManagedBy, hcoutil.OperatorName))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelVersion, hcoutil.GetHcoKvIoVersion()))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelPartOf, hcoutil.HyperConvergedCluster))
 			Expect(foundResource.Namespace).To(Equal(expectedResource.Namespace))
 			Expect(reflect.DeepEqual(expectedResource.Spec, foundResource.Spec)).To(BeTrue())
 		})
@@ -252,12 +259,59 @@ var _ = Describe("Kubevirt Console Plugin", func() {
 			Expect(hco.Status.RelatedObjects).To(ContainElement(*objectRef))
 		})
 
-		It("should reconcile deployment to default if changed", func() {
+		It("should reconcile deployment to default if changed - (updatable fields)", func() {
 			expectedResource, _ := NewKvUIPluginDeplymnt(hco)
 			outdatedResource, _ := NewKvUIPluginDeplymnt(hco)
 
-			outdatedResource.ObjectMeta.Labels[hcoutil.AppLabel] = "wrong label"
+			outdatedResource.ObjectMeta.UID = "oldObjectUID"
+			outdatedResource.ObjectMeta.ResourceVersion = "1234"
+
+			outdatedResource.Spec.Replicas = pointer.Int32(123)
 			outdatedResource.Spec.Template.Spec.Containers[0].Image = "quay.io/fake/image:latest"
+
+			cl := commontestutils.InitClient([]client.Object{hco, outdatedResource})
+			handler, err := newKvUIPluginDeploymentHandler(logger, cl, commontestutils.GetScheme(), hco)
+			Expect(err).ToNot(HaveOccurred())
+			res := handler[0].ensure(req)
+			Expect(res.UpgradeDone).To(BeFalse())
+			Expect(res.Updated).To(BeTrue())
+			Expect(res.Err).ToNot(HaveOccurred())
+
+			foundResource := &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Name: expectedResource.Name, Namespace: expectedResource.Namespace},
+					foundResource),
+			).ToNot(HaveOccurred())
+
+			Expect(foundResource.Spec.Replicas).ToNot(Equal(outdatedResource.Spec.Replicas))
+			Expect(foundResource.Spec.Replicas).To(Equal(expectedResource.Spec.Replicas))
+			Expect(foundResource.Spec.Template.Spec.Containers[0].Image).To(Equal(expectedResource.Spec.Template.Spec.Containers[0].Image))
+			Expect(reflect.DeepEqual(expectedResource.Spec, foundResource.Spec)).To(BeTrue())
+
+			// ObjectReference should have been updated
+			Expect(hco.Status.RelatedObjects).To(Not(BeNil()))
+			objectRefOutdated, err := reference.GetReference(commontestutils.GetScheme(), outdatedResource)
+			Expect(err).ToNot(HaveOccurred())
+			objectRefFound, err := reference.GetReference(commontestutils.GetScheme(), foundResource)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(hco.Status.RelatedObjects).To(Not(ContainElement(*objectRefOutdated)))
+			Expect(hco.Status.RelatedObjects).To(ContainElement(*objectRefFound))
+
+			// let's check the object UID to ensure that the object get updated and not deleted and recreated
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+		})
+
+		It("should reconcile deployment to default if changed - (immutable fields)", func() {
+			expectedResource, _ := NewKvUIPluginDeplymnt(hco)
+			outdatedResource, _ := NewKvUIPluginDeplymnt(hco)
+
+			outdatedResource.ObjectMeta.UID = "oldObjectUID"
+			outdatedResource.ObjectMeta.ResourceVersion = "1234"
+
+			outdatedResource.ObjectMeta.Labels[hcoutil.AppLabel] = "wrong label"
+			outdatedResource.Spec.Selector.MatchLabels[hcoutil.AppLabel] = "wrong label"
+			outdatedResource.Spec.Template.Labels[hcoutil.AppLabel] = "wrong label"
 
 			cl := commontestutils.InitClient([]client.Object{hco, outdatedResource})
 			handler, err := newKvUIPluginDeploymentHandler(logger, cl, commontestutils.GetScheme(), hco)
@@ -286,6 +340,9 @@ var _ = Describe("Kubevirt Console Plugin", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(hco.Status.RelatedObjects).To(Not(ContainElement(*objectRefOutdated)))
 			Expect(hco.Status.RelatedObjects).To(ContainElement(*objectRefFound))
+
+			// let's check the object UID to ensure that the object get really deleted and recreated
+			Expect(foundResource.GetUID()).ToNot(Equal(types.UID("oldObjectUID")))
 		})
 	})
 

kubevirt/hyperconverged-cluster-operator

controllers/operands/deploymentHandler_test.go
@@ -1,0 +1,121 @@
+package operands
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
+	hcov1beta1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
+	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/common"
+	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/commonTestUtils"
+)
+
+var _ = Describe("Deployment Handler", func() {
+	Context("update or recreate the Deployment as required", func() {
+		var hco *hcov1beta1.HyperConverged
+		var req *common.HcoRequest
+		var expectedDeployment *appsv1.Deployment
+
+		BeforeEach(func() {
+			hco = commonTestUtils.NewHco()
+			req = commonTestUtils.NewReq(hco)
+
+			expectedDeployment = &appsv1.Deployment{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Deployment",
+					APIVersion: "apps/v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   "modifiedDeployment",
+					Labels: map[string]string{"key1": "value1"},
+				},
+				Spec: appsv1.DeploymentSpec{
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{"key1": "value1"},
+					},
+				},
+			}
+		})
+
+		It("should recreate the Deployment as LabelSelector has changed", func() {
+			modifiedDeployment := &appsv1.Deployment{}
+			expectedDeployment.DeepCopyInto(modifiedDeployment)
+			// modify the LabelSelector
+			modifiedDeployment.Spec.Selector = &metav1.LabelSelector{
+				MatchLabels: map[string]string{"key2": "value2"},
+			}
+			modifiedDeployment.ObjectMeta.UID = "oldObjectUID"
+
+			// let's initialize the fake client with a modified object
+			cl := commonTestUtils.InitClient([]runtime.Object{modifiedDeployment})
+
+			foundResource := &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+
+			// let's ensure the handler properly reconcile it back to the expected state
+			handler := newDeploymentHandler(cl, commonTestUtils.GetScheme(), expectedDeployment)
+			res := handler.ensure(req)
+			Expect(res.Updated).To(BeTrue())
+			Expect(res.Err).ToNot(HaveOccurred())
+
+			foundResource = &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+
+			Expect(foundResource.Spec.Selector).Should(Equal(expectedDeployment.Spec.Selector))
+			// let's check the object UID to ensure that the object get really deleted and recreated
+			Expect(foundResource.ObjectMeta.UID).ToNot(Equal(modifiedDeployment.ObjectMeta.UID))
+		})
+
+		It("should only update, not recreate, the Deployment since LabelSelector hasn't changed", func() {
+			modifiedDeployment := &appsv1.Deployment{}
+			expectedDeployment.DeepCopyInto(modifiedDeployment)
+			// modify only the labels
+			gotLabels := modifiedDeployment.GetLabels()
+			gotLabels["key2"] = "value2"
+			modifiedDeployment.SetLabels(gotLabels)
+			modifiedDeployment.ObjectMeta.UID = "oldObjectUID"
+
+			// let's initialize the fake client with a modified object
+			cl := commonTestUtils.InitClient([]runtime.Object{modifiedDeployment})
+			foundResource := &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+
+			// let's ensure the handler properly reconcile it back to the expected state
+			handler := newDeploymentHandler(cl, commonTestUtils.GetScheme(), expectedDeployment)
+			res := handler.ensure(req)
+			Expect(res.Updated).To(BeTrue())
+			Expect(res.Err).ToNot(HaveOccurred())
+
+			foundResource = &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Namespace: modifiedDeployment.GetNamespace(), Name: modifiedDeployment.GetName()},
+					foundResource),
+			).ToNot(HaveOccurred())
+
+			Expect(foundResource.Spec.Selector).Should(Equal(expectedDeployment.Spec.Selector))
+			// let's check the object UID to ensure that the object get updated and not deleted and recreated
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+		})
+	})
+
+})

controllers/operands/kubevirtConsolePlugin_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"reflect"
 
+	"k8s.io/utils/pointer"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	consolev1 "github.com/openshift/api/console/v1"
@@ -221,6 +223,11 @@ var _ = Describe("Kubevirt Console Plugin", func() {
 			).ToNot(HaveOccurred())
 			Expect(foundResource.Name).To(Equal(expectedResource.Name))
 			Expect(foundResource.Labels).Should(HaveKeyWithValue(hcoutil.AppLabel, commonTestUtils.Name))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabel, commonTestUtils.Name))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelComponent, string(hcoutil.AppComponentDeployment)))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelManagedBy, hcoutil.OperatorName))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelVersion, hcoutil.GetHcoKvIoVersion()))
+			Expect(foundResource.Spec.Template.Labels).Should(HaveKeyWithValue(hcoutil.AppLabelPartOf, hcoutil.HyperConvergedCluster))
 			Expect(foundResource.Namespace).To(Equal(expectedResource.Namespace))
 			Expect(reflect.DeepEqual(expectedResource.Spec, foundResource.Spec)).To(BeTrue())
 		})
@@ -255,9 +262,55 @@ var _ = Describe("Kubevirt Console Plugin", func() {
 		It("should reconcile deployment to default if changed", func() {
 			expectedResource, _ := NewKvUiPluginDeplymnt(hco)
 			outdatedResource, _ := NewKvUiPluginDeplymnt(hco)
+			outdatedResource.ObjectMeta.UID = "oldObjectUID"
+			outdatedResource.ObjectMeta.ResourceVersion = "1234"
+
+			outdatedResource.Spec.Replicas = pointer.Int32(123)
+			outdatedResource.Spec.Template.Spec.Containers[0].Image = "quay.io/fake/image:latest"
+
+			cl := commonTestUtils.InitClient([]runtime.Object{hco, outdatedResource})
+			handler, err := newKvUiPluginDplymntHandler(logger, cl, commonTestUtils.GetScheme(), hco)
+			Expect(err).ToNot(HaveOccurred())
+			res := handler[0].ensure(req)
+			Expect(res.UpgradeDone).To(BeFalse())
+			Expect(res.Updated).To(BeTrue())
+			Expect(res.Err).ToNot(HaveOccurred())
+
+			foundResource := &appsv1.Deployment{}
+			Expect(
+				cl.Get(context.TODO(),
+					types.NamespacedName{Name: expectedResource.Name, Namespace: expectedResource.Namespace},
+					foundResource),
+			).ToNot(HaveOccurred())
+
+			Expect(foundResource.Spec.Replicas).ToNot(Equal(outdatedResource.Spec.Replicas))
+			Expect(foundResource.Spec.Replicas).To(Equal(expectedResource.Spec.Replicas))
+			Expect(foundResource.Spec.Template.Spec.Containers[0].Image).To(Equal(expectedResource.Spec.Template.Spec.Containers[0].Image))
+			Expect(reflect.DeepEqual(expectedResource.Spec, foundResource.Spec)).To(BeTrue())
+
+			// ObjectReference should have been updated
+			Expect(hco.Status.RelatedObjects).To(Not(BeNil()))
+			objectRefOutdated, err := reference.GetReference(commonTestUtils.GetScheme(), outdatedResource)
+			Expect(err).ToNot(HaveOccurred())
+			objectRefFound, err := reference.GetReference(commonTestUtils.GetScheme(), foundResource)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(hco.Status.RelatedObjects).To(Not(ContainElement(*objectRefOutdated)))
+			Expect(hco.Status.RelatedObjects).To(ContainElement(*objectRefFound))
+
+			// let's check the object UID to ensure that the object get updated and not deleted and recreated
+			Expect(foundResource.GetUID()).To(Equal(types.UID("oldObjectUID")))
+		})
+
+		It("should reconcile deployment to default if changed - (immutable fields)", func() {
+			expectedResource, _ := NewKvUiPluginDeplymnt(hco)
+			outdatedResource, _ := NewKvUiPluginDeplymnt(hco)
+
+			outdatedResource.ObjectMeta.UID = "oldObjectUID"
+			outdatedResource.ObjectMeta.ResourceVersion = "1234"
 
 			outdatedResource.ObjectMeta.Labels[hcoutil.AppLabel] = "wrong label"
-			outdatedResource.Spec.Template.Spec.Containers[0].Image = "quay.io/fake/image:latest"
+			outdatedResource.Spec.Selector.MatchLabels[hcoutil.AppLabel] = "wrong label"
+			outdatedResource.Spec.Template.Labels[hcoutil.AppLabel] = "wrong label"
 
 			cl := commonTestUtils.InitClient([]runtime.Object{hco, outdatedResource})
 			handler, err := newKvUiPluginDplymntHandler(logger, cl, commonTestUtils.GetScheme(), hco)
@@ -286,6 +339,9 @@ var _ = Describe("Kubevirt Console Plugin", func() {
 			Expect(err).ToNot(HaveOccurred())
 			Expect(hco.Status.RelatedObjects).To(Not(ContainElement(*objectRefOutdated)))
 			Expect(hco.Status.RelatedObjects).To(ContainElement(*objectRefFound))
+
+			// let's check the object UID to ensure that the object get really deleted and recreated
+			Expect(foundResource.GetUID()).ToNot(Equal(types.UID("oldObjectUID")))
 		})
 	})
 