kubevirt/kubevirt

commit 575e12f15fd70149a207200619d63b7de93f423c

Deprecate mediatedDevicesTypes in favor of mediatedDeviceTypes (#2402)
With github.com/kubevirt/kubevirt/pull/8525
mediatedDevicesTypes got deprecated in favor of mediatedDeviceTypes
on kubevirt/kubevirt.
Let's do the same on the HCO side for consistency.

The mutating webhook will take care of the migration
to the new API and the validating webhook will
prevent inconsistencies between the two APIs.

Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=2054863

Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

api/v1beta1/hyperconverged_types.go
@@ -429,7 +429,13 @@ type MediatedHostDevice struct {
 // +k8s:openapi-gen=true
 type MediatedDevicesConfiguration struct {
 	// +listType=atomic
+	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
+
+	// Deprecated: please use mediatedDeviceTypes instead.
+	// +optional
+	// +listType=atomic
 	MediatedDevicesTypes []string `json:"mediatedDevicesTypes,omitempty"`
+
 	// +optional
 	// +listType=atomic
 	NodeMediatedDeviceTypes []NodeMediatedDeviceTypesConfig `json:"nodeMediatedDeviceTypes,omitempty"`
@@ -442,6 +448,12 @@ type NodeMediatedDeviceTypesConfig struct {
 	// Selector which must match a node's labels for the vmi to be scheduled on that node.
 	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
 	NodeSelector map[string]string `json:"nodeSelector"`
+
+	// +listType=atomic
+	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
+
+	// Deprecated: please use mediatedDeviceTypes instead.
+	// +optional
 	// +listType=atomic
 	MediatedDevicesTypes []string `json:"mediatedDevicesTypes"`
 }

api/v1beta1/zz_generated.openapi.go
@@ -782,7 +782,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 				Description: "MediatedDevicesConfiguration holds information about MDEV types to be defined, if available",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
-					"mediatedDevicesTypes": {
+					"mediatedDeviceTypes": {
 						VendorExtensible: spec.VendorExtensible{
 							Extensions: spec.Extensions{
 								"x-kubernetes-list-type": "atomic",
@@ -790,6 +790,26 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 						},
 						SchemaProps: spec.SchemaProps{
 							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
+					"mediatedDevicesTypes": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "atomic",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "Deprecated: please use mediatedDeviceTypes instead.",
+							Type:        []string{"array"},
 							Items: &spec.SchemaOrArray{
 								Schema: &spec.Schema{
 									SchemaProps: spec.SchemaProps{
@@ -820,6 +840,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 						},
 					},
 				},
+				Required: []string{"mediatedDeviceTypes"},
 			},
 		},
 		Dependencies: []string{
@@ -894,7 +915,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_NodeMediatedDev
 							},
 						},
 					},
-					"mediatedDevicesTypes": {
+					"mediatedDeviceTypes": {
 						VendorExtensible: spec.VendorExtensible{
 							Extensions: spec.Extensions{
 								"x-kubernetes-list-type": "atomic",
@@ -913,8 +934,28 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_NodeMediatedDev
 							},
 						},
 					},
+					"mediatedDevicesTypes": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "atomic",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "Deprecated: please use mediatedDeviceTypes instead.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
 				},
-				Required: []string{"nodeSelector", "mediatedDevicesTypes"},
+				Required: []string{"nodeSelector", "mediatedDeviceTypes"},
 			},
 		},
 	}

config/crd/bases/hco.kubevirt.io_hyperconvergeds.yaml
@@ -2204,7 +2204,13 @@ spec:
                 description: MediatedDevicesConfiguration holds information about
                   MDEV types to be defined on nodes, if available
                 properties:
+                  mediatedDeviceTypes:
+                    items:
+                      type: string
+                    type: array
+                    x-kubernetes-list-type: atomic
                   mediatedDevicesTypes:
+                    description: 'Deprecated: please use mediatedDeviceTypes instead.'
                     items:
                       type: string
                     type: array
@@ -2215,7 +2221,14 @@ spec:
                         about MDEV types to be defined in a specific node that matches
                         the NodeSelector field.
                       properties:
+                        mediatedDeviceTypes:
+                          items:
+                            type: string
+                          type: array
+                          x-kubernetes-list-type: atomic
                         mediatedDevicesTypes:
+                          description: 'Deprecated: please use mediatedDeviceTypes
+                            instead.'
                           items:
                             type: string
                           type: array
@@ -2229,11 +2242,13 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDevicesTypes
+                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
                     type: array
                     x-kubernetes-list-type: atomic
+                required:
+                - mediatedDeviceTypes
                 type: object
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete

deploy/crds/hco00.crd.yaml
@@ -2204,7 +2204,13 @@ spec:
                 description: MediatedDevicesConfiguration holds information about
                   MDEV types to be defined on nodes, if available
                 properties:
+                  mediatedDeviceTypes:
+                    items:
+                      type: string
+                    type: array
+                    x-kubernetes-list-type: atomic
                   mediatedDevicesTypes:
+                    description: 'Deprecated: please use mediatedDeviceTypes instead.'
                     items:
                       type: string
                     type: array
@@ -2215,7 +2221,14 @@ spec:
                         about MDEV types to be defined in a specific node that matches
                         the NodeSelector field.
                       properties:
+                        mediatedDeviceTypes:
+                          items:
+                            type: string
+                          type: array
+                          x-kubernetes-list-type: atomic
                         mediatedDevicesTypes:
+                          description: 'Deprecated: please use mediatedDeviceTypes
+                            instead.'
                           items:
                             type: string
                           type: array
@@ -2229,11 +2242,13 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDevicesTypes
+                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
                     type: array
                     x-kubernetes-list-type: atomic
+                required:
+                - mediatedDeviceTypes
                 type: object
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete

tests/vendor/github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1/hyperconverged_types.go
@@ -429,7 +429,13 @@ type MediatedHostDevice struct {
 // +k8s:openapi-gen=true
 type MediatedDevicesConfiguration struct {
 	// +listType=atomic
+	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
+
+	// Deprecated: please use mediatedDeviceTypes instead.
+	// +optional
+	// +listType=atomic
 	MediatedDevicesTypes []string `json:"mediatedDevicesTypes,omitempty"`
+
 	// +optional
 	// +listType=atomic
 	NodeMediatedDeviceTypes []NodeMediatedDeviceTypesConfig `json:"nodeMediatedDeviceTypes,omitempty"`
@@ -442,6 +448,12 @@ type NodeMediatedDeviceTypesConfig struct {
 	// Selector which must match a node's labels for the vmi to be scheduled on that node.
 	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
 	NodeSelector map[string]string `json:"nodeSelector"`
+
+	// +listType=atomic
+	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
+
+	// Deprecated: please use mediatedDeviceTypes instead.
+	// +optional
 	// +listType=atomic
 	MediatedDevicesTypes []string `json:"mediatedDevicesTypes"`
 }

tests/vendor/github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1/zz_generated.openapi.go
@@ -782,7 +782,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 				Description: "MediatedDevicesConfiguration holds information about MDEV types to be defined, if available",
 				Type:        []string{"object"},
 				Properties: map[string]spec.Schema{
-					"mediatedDevicesTypes": {
+					"mediatedDeviceTypes": {
 						VendorExtensible: spec.VendorExtensible{
 							Extensions: spec.Extensions{
 								"x-kubernetes-list-type": "atomic",
@@ -790,6 +790,26 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 						},
 						SchemaProps: spec.SchemaProps{
 							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
+					"mediatedDevicesTypes": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "atomic",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "Deprecated: please use mediatedDeviceTypes instead.",
+							Type:        []string{"array"},
 							Items: &spec.SchemaOrArray{
 								Schema: &spec.Schema{
 									SchemaProps: spec.SchemaProps{
@@ -820,6 +840,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 						},
 					},
 				},
+				Required: []string{"mediatedDeviceTypes"},
 			},
 		},
 		Dependencies: []string{
@@ -894,7 +915,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_NodeMediatedDev
 							},
 						},
 					},
-					"mediatedDevicesTypes": {
+					"mediatedDeviceTypes": {
 						VendorExtensible: spec.VendorExtensible{
 							Extensions: spec.Extensions{
 								"x-kubernetes-list-type": "atomic",
@@ -913,8 +934,28 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_NodeMediatedDev
 							},
 						},
 					},
+					"mediatedDevicesTypes": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-type": "atomic",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "Deprecated: please use mediatedDeviceTypes instead.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: "",
+										Type:    []string{"string"},
+										Format:  "",
+									},
+								},
+							},
+						},
+					},
 				},
-				Required: []string{"nodeSelector", "mediatedDevicesTypes"},
+				Required: []string{"nodeSelector", "mediatedDeviceTypes"},
 			},
 		},
 	}

deploy/index-image/community-kubevirt-hyperconverged/1.10.0/manifests/hco00.crd.yaml
@@ -2204,7 +2204,13 @@ spec:
                 description: MediatedDevicesConfiguration holds information about
                   MDEV types to be defined on nodes, if available
                 properties:
+                  mediatedDeviceTypes:
+                    items:
+                      type: string
+                    type: array
+                    x-kubernetes-list-type: atomic
                   mediatedDevicesTypes:
+                    description: 'Deprecated: please use mediatedDeviceTypes instead.'
                     items:
                       type: string
                     type: array
@@ -2215,7 +2221,14 @@ spec:
                         about MDEV types to be defined in a specific node that matches
                         the NodeSelector field.
                       properties:
+                        mediatedDeviceTypes:
+                          items:
+                            type: string
+                          type: array
+                          x-kubernetes-list-type: atomic
                         mediatedDevicesTypes:
+                          description: 'Deprecated: please use mediatedDeviceTypes
+                            instead.'
                           items:
                             type: string
                           type: array
@@ -2229,11 +2242,13 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDevicesTypes
+                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
                     type: array
                     x-kubernetes-list-type: atomic
+                required:
+                - mediatedDeviceTypes
                 type: object
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete

deploy/olm-catalog/community-kubevirt-hyperconverged/1.10.0/manifests/hco00.crd.yaml
@@ -2204,7 +2204,13 @@ spec:
                 description: MediatedDevicesConfiguration holds information about
                   MDEV types to be defined on nodes, if available
                 properties:
+                  mediatedDeviceTypes:
+                    items:
+                      type: string
+                    type: array
+                    x-kubernetes-list-type: atomic
                   mediatedDevicesTypes:
+                    description: 'Deprecated: please use mediatedDeviceTypes instead.'
                     items:
                       type: string
                     type: array
@@ -2215,7 +2221,14 @@ spec:
                         about MDEV types to be defined in a specific node that matches
                         the NodeSelector field.
                       properties:
+                        mediatedDeviceTypes:
+                          items:
+                            type: string
+                          type: array
+                          x-kubernetes-list-type: atomic
                         mediatedDevicesTypes:
+                          description: 'Deprecated: please use mediatedDeviceTypes
+                            instead.'
                           items:
                             type: string
                           type: array
@@ -2229,11 +2242,13 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDevicesTypes
+                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
                     type: array
                     x-kubernetes-list-type: atomic
+                required:
+                - mediatedDeviceTypes
                 type: object
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete

kubevirt/hyperconverged-cluster-operator

commit 523e9a334e1b61bf023fe278115b926c828fcdfc

Do not require nodemediatedDeviceTypes to prevent broken upgrades
With #2402 we deprecated MediatedDevicesTypes and
NodeMediatedDeviceTypes.MediatedDevicesTypes for
MediatedDeviceTypes and NodeMediatedDeviceTypes.MediatedDeviceTypes.
We also introduced a mutating webhook to handle the conversion.
Unfortunately make the two new names mandatory is breaking
the validation of the new version of the CRD on OLM upgrades
since only the previous names were there with the old CRD.

We already have func test covering this area,
but they are fooled by the mutating webhook that
instead is not involved into the CRD validation process
on the OLM side.
On the other side, add an additional upgrade lane just for
this is probably too expensive.

Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=2241327

Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

api/v1beta1/hyperconverged_types.go
@@ -458,7 +458,9 @@ type MediatedHostDevice struct {
 
 // MediatedDevicesConfiguration holds information about MDEV types to be defined, if available
 // +k8s:openapi-gen=true
+// +kubebuilder:validation:XValidation:rule="(has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0) || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)",message="for mediatedDevicesConfiguration a non-empty mediatedDeviceTypes or mediatedDevicesTypes(deprecated) is required"
 type MediatedDevicesConfiguration struct {
+	// +optional
 	// +listType=atomic
 	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
 
@@ -474,18 +476,21 @@ type MediatedDevicesConfiguration struct {
 
 // NodeMediatedDeviceTypesConfig holds information about MDEV types to be defined in a specific node that matches the NodeSelector field.
 // +k8s:openapi-gen=true
+// +kubebuilder:validation:XValidation:rule="(has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0) || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)",message="for nodeMediatedDeviceTypes a non-empty mediatedDeviceTypes or mediatedDevicesTypes(deprecated) is required"
 type NodeMediatedDeviceTypesConfig struct {
+
 	// NodeSelector is a selector which must be true for the vmi to fit on a node.
 	// Selector which must match a node's labels for the vmi to be scheduled on that node.
 	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
 	NodeSelector map[string]string `json:"nodeSelector"`
 
 	// +listType=atomic
+	// +optional
 	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
 
 	// Deprecated: please use mediatedDeviceTypes instead.
-	// +optional
 	// +listType=atomic
+	// +optional
 	MediatedDevicesTypes []string `json:"mediatedDevicesTypes"`
 }
 

api/v1beta1/zz_generated.openapi.go
@@ -857,7 +857,6 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 						},
 					},
 				},
-				Required: []string{"mediatedDeviceTypes"},
 			},
 		},
 		Dependencies: []string{
@@ -972,7 +971,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_NodeMediatedDev
 						},
 					},
 				},
-				Required: []string{"nodeSelector", "mediatedDeviceTypes"},
+				Required: []string{"nodeSelector"},
 			},
 		},
 	}

config/crd/bases/hco.kubevirt.io_hyperconvergeds.yaml
@@ -2260,14 +2260,21 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
+                      x-kubernetes-validations:
+                      - message: for nodeMediatedDeviceTypes a non-empty mediatedDeviceTypes
+                          or mediatedDevicesTypes(deprecated) is required
+                        rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                          || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
                     type: array
                     x-kubernetes-list-type: atomic
-                required:
-                - mediatedDeviceTypes
                 type: object
+                x-kubernetes-validations:
+                - message: for mediatedDevicesConfiguration a non-empty mediatedDeviceTypes
+                    or mediatedDevicesTypes(deprecated) is required
+                  rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                    || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete
                   CPU models

deploy/crds/hco00.crd.yaml
@@ -2260,14 +2260,21 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
+                      x-kubernetes-validations:
+                      - message: for nodeMediatedDeviceTypes a non-empty mediatedDeviceTypes
+                          or mediatedDevicesTypes(deprecated) is required
+                        rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                          || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
                     type: array
                     x-kubernetes-list-type: atomic
-                required:
-                - mediatedDeviceTypes
                 type: object
+                x-kubernetes-validations:
+                - message: for mediatedDevicesConfiguration a non-empty mediatedDeviceTypes
+                    or mediatedDevicesTypes(deprecated) is required
+                  rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                    || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete
                   CPU models

deploy/index-image/community-kubevirt-hyperconverged/1.11.0/manifests/hco00.crd.yaml
@@ -2260,14 +2260,21 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
+                      x-kubernetes-validations:
+                      - message: for nodeMediatedDeviceTypes a non-empty mediatedDeviceTypes
+                          or mediatedDevicesTypes(deprecated) is required
+                        rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                          || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
                     type: array
                     x-kubernetes-list-type: atomic
-                required:
-                - mediatedDeviceTypes
                 type: object
+                x-kubernetes-validations:
+                - message: for mediatedDevicesConfiguration a non-empty mediatedDeviceTypes
+                    or mediatedDevicesTypes(deprecated) is required
+                  rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                    || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete
                   CPU models

deploy/olm-catalog/community-kubevirt-hyperconverged/1.11.0/manifests/hco00.crd.yaml
@@ -2260,14 +2260,21 @@ spec:
                             More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/'
                           type: object
                       required:
-                      - mediatedDeviceTypes
                       - nodeSelector
                       type: object
+                      x-kubernetes-validations:
+                      - message: for nodeMediatedDeviceTypes a non-empty mediatedDeviceTypes
+                          or mediatedDevicesTypes(deprecated) is required
+                        rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                          || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
                     type: array
                     x-kubernetes-list-type: atomic
-                required:
-                - mediatedDeviceTypes
                 type: object
+                x-kubernetes-validations:
+                - message: for mediatedDevicesConfiguration a non-empty mediatedDeviceTypes
+                    or mediatedDevicesTypes(deprecated) is required
+                  rule: (has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0)
+                    || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)
               obsoleteCPUs:
                 description: ObsoleteCPUs allows avoiding scheduling of VMs for obsolete
                   CPU models

tests/vendor/github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1/hyperconverged_types.go
@@ -458,7 +458,9 @@ type MediatedHostDevice struct {
 
 // MediatedDevicesConfiguration holds information about MDEV types to be defined, if available
 // +k8s:openapi-gen=true
+// +kubebuilder:validation:XValidation:rule="(has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0) || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)",message="for mediatedDevicesConfiguration a non-empty mediatedDeviceTypes or mediatedDevicesTypes(deprecated) is required"
 type MediatedDevicesConfiguration struct {
+	// +optional
 	// +listType=atomic
 	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
 
@@ -474,18 +476,21 @@ type MediatedDevicesConfiguration struct {
 
 // NodeMediatedDeviceTypesConfig holds information about MDEV types to be defined in a specific node that matches the NodeSelector field.
 // +k8s:openapi-gen=true
+// +kubebuilder:validation:XValidation:rule="(has(self.mediatedDeviceTypes) && size(self.mediatedDeviceTypes)>0) || (has(self.mediatedDevicesTypes) && size(self.mediatedDevicesTypes)>0)",message="for nodeMediatedDeviceTypes a non-empty mediatedDeviceTypes or mediatedDevicesTypes(deprecated) is required"
 type NodeMediatedDeviceTypesConfig struct {
+
 	// NodeSelector is a selector which must be true for the vmi to fit on a node.
 	// Selector which must match a node's labels for the vmi to be scheduled on that node.
 	// More info: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
 	NodeSelector map[string]string `json:"nodeSelector"`
 
 	// +listType=atomic
+	// +optional
 	MediatedDeviceTypes []string `json:"mediatedDeviceTypes"`
 
 	// Deprecated: please use mediatedDeviceTypes instead.
-	// +optional
 	// +listType=atomic
+	// +optional
 	MediatedDevicesTypes []string `json:"mediatedDevicesTypes"`
 }
 

tests/vendor/github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1/zz_generated.openapi.go
@@ -857,7 +857,6 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_MediatedDevices
 						},
 					},
 				},
-				Required: []string{"mediatedDeviceTypes"},
 			},
 		},
 		Dependencies: []string{
@@ -972,7 +971,7 @@ func schema_kubevirt_hyperconverged_cluster_operator_api_v1beta1_NodeMediatedDev
 						},
 					},
 				},
-				Required: []string{"nodeSelector", "mediatedDeviceTypes"},
+				Required: []string{"nodeSelector"},
 			},
 		},
 	}