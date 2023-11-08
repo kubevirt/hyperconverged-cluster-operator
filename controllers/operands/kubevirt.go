89f636b5ca931f9e4de580b2276606c09a6721e3
Alona Paz <alkaplan@redhat.com>
2023-11-08 11:35:31 +0200

    Enable KubeVirt network binding plugins feature gate
    Signed-off-by: Alona Paz <alkaplan@redhat.com>

--- controllers/operands/kubevirt.go
+++ controllers/operands/kubevirt.go
@@ -107,6 +107,9 @@ const (
 
 	// Enable VM state persistence
 	kvVMPersistentState = "VMPersistentState"
+
+	// Enable using a plugin to bind the pod and the VM network
+	kvHNetworkBindingPluginsGate = "NetworkBindingPlugins"
 )
 
 const (
@@ -132,6 +135,7 @@ var (
 		kvKubevirtSeccompProfile,
 		kvHotplugNicsGate,
 		kvVMPersistentState,
+		kvHNetworkBindingPluginsGate,
 	}
 
 	// holds a list of mandatory KubeVirt feature gates. Some of them are the hard coded feature gates and some of
