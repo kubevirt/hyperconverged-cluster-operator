995b791096cdd8c236e6e1045fe4adf8cb4f10a4
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2024-02-08 15:31:10 +0200

    Handle the kubevirt-template-validator image in CSV
    The kubevirt-template-validator was missing in the related-image list in
    the CSV. Also, it was listed with tag instead of with digest, in the SSP
    deployment.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- tools/csv-merger/csv-merger.go
+++ tools/csv-merger/csv-merger.go
@@ -589,7 +589,7 @@ func appendOnce(slice []string, item string) []string {
 
 func appendRelatedImageIfMissing(slice []csvv1alpha1.RelatedImage, ri csvv1alpha1.RelatedImage) []csvv1alpha1.RelatedImage {
 	for _, ele := range slice {
-		if ele.Name == ri.Name {
+		if ele.Image == ri.Image {
 			return slice
 		}
 	}
