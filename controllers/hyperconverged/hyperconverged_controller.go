e56d04f7b33b1c2e8f2e175769f8d1d0e7987fc5
João Vilaça <machadovilaca@gmail.com>
2025-01-09 12:30:59 +0000

    Refactor hyperconverged monitoring into separate package
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

--- controllers/hyperconverged/hyperconverged_controller.go
+++ controllers/hyperconverged/hyperconverged_controller.go
@@ -52,7 +52,7 @@ import (
 	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/alerts"
 	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/common"
 	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/operands"
-	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/monitoring/metrics"
+	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/monitoring/hyperconverged/metrics"
 	hcoutil "github.com/kubevirt/hyperconverged-cluster-operator/pkg/util"
 	"github.com/kubevirt/hyperconverged-cluster-operator/version"
 )
