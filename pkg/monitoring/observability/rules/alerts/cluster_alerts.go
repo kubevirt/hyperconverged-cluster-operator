b89d5e86a8440654b4cc4231dc6960bf0ce37888
João Vilaça <machadovilaca@gmail.com>
2025-01-13 16:38:55 +0000

    Add HAControlPlaneDown alert
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

--- pkg/monitoring/observability/rules/alerts/cluster_alerts.go
+++ pkg/monitoring/observability/rules/alerts/cluster_alerts.go
@@ -21,5 +21,18 @@ func clusterAlerts() []promv1.Rule {
 				"operator_health_impact": "none",
 			},
 		},
+		{
+			Alert: "HAControlPlaneDown",
+			Expr:  intstr.FromString("kube_node_role{role=\"control-plane\"} * on(node) kube_node_status_condition{condition=\"Ready\",status=\"true\"} == 0"),
+			For:   ptr.To(promv1.Duration("5m")),
+			Annotations: map[string]string{
+				"summary":     "Control plane node {{ $labels.node }} is not ready",
+				"description": "Control plane node {{ $labels.node }} has been not ready for more than 5 minutes.",
+			},
+			Labels: map[string]string{
+				"severity":               "critical",
+				"operator_health_impact": "none",
+			},
+		},
 	}
 }
