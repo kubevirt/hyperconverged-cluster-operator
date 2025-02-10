cc0e23a8f0072aa9d4a81885ac51ac1bf5263b95
João Vilaça <machadovilaca@gmail.com>
2025-02-10 19:05:24 +0000

    Require Bearer token for /metrics endpoint
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

---
+++ pkg/authorization/bearer_token_test.go
@@ -0,0 +1,57 @@
+package authorization_test
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/authorization"
+)
+
+var _ = Describe("BearerToken", func() {
+	BeforeEach(func() {
+		tmpFile, err := os.CreateTemp("", "fake-token")
+		Expect(err).ToNot(HaveOccurred())
+
+		_, err = tmpFile.Write([]byte("test-secret-key"))
+		Expect(err).ToNot(HaveOccurred())
+
+		os.Setenv(authorization.TokenPathEnvVar, tmpFile.Name())
+	})
+
+	AfterEach(func() {
+		tokenPath := os.Getenv(authorization.TokenPathEnvVar)
+		if tokenPath != "" {
+			os.Remove(tokenPath)
+		}
+		os.Unsetenv(authorization.TokenPathEnvVar)
+	})
+
+	Context("with a valid token", func() {
+		It("should create and validate a token", func() {
+			token, err := authorization.CreateToken()
+			Expect(err).ToNot(HaveOccurred())
+			Expect(token).ToNot(BeEmpty())
+
+			valid, err := authorization.ValidateToken(token)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(valid).To(BeTrue())
+		})
+	})
+
+	Context("with an invalid token", func() {
+		It("should fail validation for malformed token", func() {
+			valid, err := authorization.ValidateToken("invalid-token")
+			Expect(err).To(HaveOccurred())
+			Expect(valid).To(BeFalse())
+		})
+
+		It("should fail validation for empty token", func() {
+			valid, err := authorization.ValidateToken("")
+			Expect(err).To(HaveOccurred())
+			Expect(valid).To(BeFalse())
+		})
+	})
+
+})
