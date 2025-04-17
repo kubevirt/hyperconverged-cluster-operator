b4e6800650266548cc832ff6d020ae9df89632a3
João Vilaça <machadovilaca@gmail.com>
2025-04-17 20:55:40 +0100

    [release-1.14] Add observability controller alerts (#3385)
    * Refactor hyperconverged monitoring into separate package
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Add base for observability controller Prometheus rules
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Add HighCPUWorkload alert
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Add HAControlPlaneDown alert
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Add NodeNetworkInterfaceDown alert
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Update observability func tests
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Add observability controller unit tests
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Add new ignored interfaces to NodeNetworkInterfaceDown Alert
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Reduce NodeNetworkInterfaceDown chance for false positives
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Refactor pointer conversions in cluster alerts
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    ---------
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

--- tests/func-tests/observability_controller_test.go
+++ tests/func-tests/observability_controller_test.go
@@ -10,23 +10,30 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/observability"
+	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/alertmanager"
 	tests "github.com/kubevirt/hyperconverged-cluster-operator/tests/func-tests"
 )
 
 const testName = "observability_controller"
 
 var _ = Describe("Observability Controller", Label(tests.OpenshiftLabel, testName), func() {
+	var cli client.Client
+
+	BeforeEach(func(ctx context.Context) {
+		cli = tests.GetControllerRuntimeClient()
+		tests.FailIfNotOpenShift(ctx, cli, testName)
+	})
+
 	Context("PodDisruptionBudgetAtLimit", func() {
 		BeforeEach(func(ctx context.Context) {
 			cli := tests.GetControllerRuntimeClient()
 			tests.FailIfNotOpenShift(ctx, cli, testName)
 		})
 
-		It("should be silenced", func() {
-			r := observability.NewReconciler(tests.GetClientConfig())
-
-			amApi, err := r.NewAlertmanagerApi()
+		It("should be silenced", func(ctx context.Context) {
+			httpClient, err := observability.NewHTTPClient()
 			Expect(err).ToNot(HaveOccurred())
+			amApi := alertmanager.NewAPI(*httpClient, observability.AlertmanagerSvcHost, tests.GetClientConfig().BearerToken)
 
 			amSilences, err := amApi.ListSilences()
 			Expect(err).ToNot(HaveOccurred())
@@ -39,16 +46,15 @@ var _ = Describe("Observability Controller", Label(tests.OpenshiftLabel, testNam
 			Expect(err).ToNot(HaveOccurred())
 
 			// Restart pod to force reconcile (reconcile periodicity is 1h)
-			cli := tests.GetControllerRuntimeClient()
 			var hcoPods v1.PodList
-			err = cli.List(context.Background(), &hcoPods, &client.MatchingLabels{
+			err = cli.List(ctx, &hcoPods, &client.MatchingLabels{
 				"name": "hyperconverged-cluster-operator",
 			})
 			Expect(err).ToNot(HaveOccurred())
 			Expect(hcoPods.Items).ToNot(BeEmpty())
 
 			for _, pod := range hcoPods.Items {
-				err = cli.Delete(context.Background(), &pod)
+				err = cli.Delete(ctx, &pod)
 				Expect(err).ToNot(HaveOccurred())
 			}
 
