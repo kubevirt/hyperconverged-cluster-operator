63a4bbfb6a750b486a9108d409351ffe709a517c
Or Shoval <oshoval@redhat.com>
2025-06-25 17:23:04 +0300

    passt: Add ServiceAccountHandler
    Use it to conditionally deploy ServiceAccount needed for Passt.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- controllers/handlers/passt/passt.go
+++ controllers/handlers/passt/passt.go
@@ -7,9 +7,14 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kubevirtcorev1 "kubevirt.io/api/core/v1"
 
+	hcov1beta1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
+	"github.com/kubevirt/hyperconverged-cluster-operator/controllers/operands"
 	hcoutil "github.com/kubevirt/hyperconverged-cluster-operator/pkg/util"
 )
 
@@ -56,9 +61,43 @@ func NetworkBinding(sidecarImage string) kubevirtcorev1.InterfaceBindingPlugin {
 	}
 }
 
+// GetImage gets the passt image from environment variable
 func GetImage() string {
 	passtImageOnce.Do(func() {
 		passtImage = os.Getenv(hcoutil.PasstImageEnvV)
 	})
 	return passtImage
+}
+
+// NewPasstBindingCNISA creates a ServiceAccount for the passt binding CNI
+func NewPasstBindingCNISA(hc *hcov1beta1.HyperConverged) *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "passt-binding-cni",
+			Namespace: hc.Namespace,
+			Labels:    hcoutil.GetLabels(hcoutil.HyperConvergedName, hcoutil.AppComponentNetwork),
+		},
+	}
+}
+
+// NewPasstServiceAccountHandler creates a conditional handler for passt ServiceAccount
+func NewPasstServiceAccountHandler(Client client.Client, Scheme *runtime.Scheme) operands.Operand {
+	return createPasstConditionalHandler(
+		operands.NewServiceAccountHandler(Client, Scheme, NewPasstBindingCNISA),
+		func(hc *hcov1beta1.HyperConverged) client.Object {
+			return NewPasstBindingCNISA(hc)
+		},
+	)
+}
+
+// createPasstConditionalHandler creates a conditional handler that checks for passt deployment annotation
+func createPasstConditionalHandler(handler *operands.GenericOperand, objectCreator func(hc *hcov1beta1.HyperConverged) client.Object) operands.Operand {
+	return operands.NewConditionalHandler(
+		handler,
+		func(hc *hcov1beta1.HyperConverged) bool {
+			value, ok := hc.Annotations[DeployPasstNetworkBindingAnnotation]
+			return ok && value == "true"
+		},
+		objectCreator,
+	)
 }
