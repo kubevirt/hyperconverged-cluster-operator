aedd9b5b8e11c83557ed664353d50383fdef28c1
dsionov <dsionov@redhat.com>
2025-07-10 01:23:22 +0300

    api: expose VideoConfig feature gate in HCO
    Expose the VideoConfig feature gate in the HCO API, allowing users
    to enable or disable it via the HyperConverged CR.
    
    This follows the merge of kubevirt/kubevirt#14673, which introduced
    the feature in KubeVirt. Since this is an alpha feature, it remains
    disabled by default.
    
    Signed-off-by: Daniel Sionov <dsionov@redhat.com>

--- api/v1beta1/zz_generated.defaults.go
+++ api/v1beta1/zz_generated.defaults.go
@@ -64,6 +64,10 @@ func SetObjectDefaults_HyperConverged(in *HyperConverged) {
 		var ptrVar1 bool = false
 		in.Spec.FeatureGates.EnableMultiArchCommonBootImageImport = &ptrVar1
 	}
+	if in.Spec.FeatureGates.VideoConfig == nil {
+		var ptrVar1 bool = false
+		in.Spec.FeatureGates.VideoConfig = &ptrVar1
+	}
 	if in.Spec.LiveMigrationConfig.ParallelMigrationsPerCluster == nil {
 		var ptrVar1 uint32 = 5
 		in.Spec.LiveMigrationConfig.ParallelMigrationsPerCluster = &ptrVar1
