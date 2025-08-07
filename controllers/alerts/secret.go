c6033695b34dd7150b84d1a2e9a1128a9c1cfb3c
João Vilaça <machadovilaca@gmail.com>
2025-08-07 16:45:59 +0100

    Force recreate metrics endpoint secret to ensure Prometheus config reload (#3688)
    * Force recreate metrics endpoint secret
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    * Avoid reconciler nil objects
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>
    
    ---------
    
    Signed-off-by: João Vilaça <machadovilaca@gmail.com>

--- controllers/alerts/secret.go
+++ controllers/alerts/secret.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -21,8 +22,14 @@ type secretReconciler struct {
 }
 
 func newSecretReconciler(namespace string, owner metav1.OwnerReference) *secretReconciler {
+	token, err := authorization.CreateToken()
+	if err != nil {
+		logger.Error(err, "failed to create bearer token")
+		return nil
+	}
+
 	return &secretReconciler{
-		theSecret: NewSecret(namespace, owner),
+		theSecret: NewSecret(namespace, owner, token),
 	}
 }
 
@@ -42,42 +49,41 @@ func (r *secretReconciler) EmptyObject() client.Object {
 	return &corev1.Secret{}
 }
 
+// UpdateExistingResource checks if the secret already exists and has the correct token.
+// If it does, it returns nil. If the secret exists but the token is incorrect, it deletes the old secret
+// and creates a new one with the updated token. If the secret does not exist, it creates a new one.
+// It deletes the old secret to force Prometheus to reload the configuration.
 func (r *secretReconciler) UpdateExistingResource(ctx context.Context, cl client.Client, resource client.Object, logger logr.Logger) (client.Object, bool, error) {
 	found := resource.(*corev1.Secret)
-	modified := false
 
 	token, err := authorization.CreateToken()
 	if err != nil {
 		return nil, false, err
 	}
 
-	if found.Data["token"] == nil || string(found.Data["token"]) != token {
-		found.StringData = map[string]string{
-			"token": token,
-		}
-		modified = true
+	// Check if the secret has the correct token
+	if string(found.Data["token"]) == token {
+		return nil, false, nil
 	}
 
-	modified = updateCommonDetails(&r.theSecret.ObjectMeta, &found.ObjectMeta) || modified
-
-	if modified {
-		err := cl.Update(ctx, found)
-		if err != nil {
-			logger.Error(err, "failed to update the Secret")
+	// If the the token is incorrect, delete the old secret and create a new one
+	if err := cl.Delete(ctx, found); err != nil {
+		if !errors.IsNotFound(err) {
+			logger.Error(err, "failed to delete old secret")
 			return nil, false, err
 		}
 	}
 
-	return found, modified, nil
-}
-
-func NewSecret(namespace string, owner metav1.OwnerReference) *corev1.Secret {
-	token, err := authorization.CreateToken()
-	if err != nil {
-		logger.Error(err, "failed to create bearer token")
-		return nil
+	newSecret := NewSecret(r.theSecret.Namespace, r.theSecret.OwnerReferences[0], token)
+	if err := cl.Create(ctx, newSecret); err != nil {
+		logger.Error(err, "failed to create new secret")
+		return nil, false, err
 	}
 
+	return newSecret, true, nil
+}
+
+func NewSecret(namespace string, owner metav1.OwnerReference, token string) *corev1.Secret {
 	return &corev1.Secret{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
