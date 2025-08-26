60d6821a0969bb868c61a3ba6fec74107f5c80da
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2025-08-26 08:54:39 +0300

    ImageStreams: Use fs.FS interface instead of the os pkg
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- controllers/handlers/imageStream.go
+++ controllers/handlers/imageStream.go
@@ -3,14 +3,14 @@ package handlers
 import (
 	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
+	"io/fs"
 	"reflect"
 	"strings"
 
 	log "github.com/go-logr/logr"
 	imagev1 "github.com/openshift/api/image/v1"
 	objectreferencesv1 "github.com/openshift/custom-resource-status/objectreferences/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/reference"
@@ -22,12 +22,9 @@ import (
 	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/util"
 )
 
-// TODO: this is very similar to the quickstart file; on golang 1.18, check if it possible to use type parameters instead.
-
 // ImageStream resources are a short user guids
 const (
-	ImageStreamManifestLocationVarName = "IMAGE_STREAM_FILES_LOCATION"
-	imageStreamDefaultManifestLocation = "./imageStreams"
+	ImageStreamDefaultManifestLocation = "imageStreams"
 )
 
 var (
@@ -238,31 +235,37 @@ func (h isHooks) addMissingTags(found *imagev1.ImageStream, newTags []imagev1.Ta
 	return newTags, modified
 }
 
-func GetImageStreamHandlers(logger log.Logger, Client client.Client, Scheme *runtime.Scheme, hc *hcov1beta1.HyperConverged) ([]operands.Operand, error) {
-	filesLocation := util.GetManifestDirPath(ImageStreamManifestLocationVarName, imageStreamDefaultManifestLocation)
-
-	err := util.ValidateManifestDir(filesLocation)
+func GetImageStreamHandlers(logger log.Logger, Client client.Client, Scheme *runtime.Scheme, hc *hcov1beta1.HyperConverged, dir fs.FS) ([]operands.Operand, error) {
+	err := util.ValidateManifestDir(ImageStreamDefaultManifestLocation, dir)
 	if err != nil {
-		logger.Error(err, "can't get manifest directory for imageStreams", "imageStream files location", filesLocation)
+		if k8serrors.IsNotFound(err) {
+			logger.Info("no imageStream files found, skipping")
+			return nil, nil
+		}
+
+		logger.Error(err, "can't get manifest directory for imageStreams", "imageStream files location", ImageStreamDefaultManifestLocation)
 		return nil, errors.Unwrap(err) // if not wrapped, then it's not an error that stops processing, and it return nil
 	}
 
-	return createImageStreamHandlersFromFiles(logger, Client, Scheme, hc, filesLocation)
+	return createImageStreamHandlersFromFiles(logger, Client, Scheme, hc, ImageStreamDefaultManifestLocation, dir)
 }
 
-func createImageStreamHandlersFromFiles(logger log.Logger, Client client.Client, Scheme *runtime.Scheme, hc *hcov1beta1.HyperConverged, filesLocation string) ([]operands.Operand, error) {
+func createImageStreamHandlersFromFiles(logger log.Logger, Client client.Client, Scheme *runtime.Scheme, hc *hcov1beta1.HyperConverged, filesLocation string, dir fs.FS) ([]operands.Operand, error) {
 	var handlers []operands.Operand
-	imageStreamNames = []string{}
 
 	logger.Info("walking over the files in " + filesLocation + ", to find imageStream files.")
 
-	err := filepath.Walk(filesLocation, func(path string, info os.FileInfo, err error) error {
+	err := fs.WalkDir(dir, filesLocation, func(path string, entry fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		logger.Info("processing imageStream file", "fileName", path, "fileInfo", info)
-		is, err := processImageStreamFile(path, info, logger, hc, Client, Scheme)
+		if entry.IsDir() || !strings.HasSuffix(entry.Name(), ".yaml") {
+			return nil
+		}
+
+		logger.Info("processing imageStream file", "fileName", path, "fileInfo", entry)
+		is, err := processImageStreamFile(path, dir, logger, hc, Client, Scheme)
 		if err != nil {
 			return err
 		}
@@ -292,29 +295,30 @@ func compareOneTag(foundTag, reqTag *imagev1.TagReference) bool {
 	return modified
 }
 
-func processImageStreamFile(path string, info os.FileInfo, logger log.Logger, hc *hcov1beta1.HyperConverged, Client client.Client, Scheme *runtime.Scheme) (operands.Operand, error) {
-	if !info.IsDir() && strings.HasSuffix(info.Name(), ".yaml") {
-		file, err := os.Open(path)
-		if err != nil {
-			logger.Error(err, "Can't open the ImageStream yaml file", "file name", path)
-			return nil, err
-		}
-
-		is := &imagev1.ImageStream{}
-		err = util.UnmarshalYamlFileToObject(file, is)
-		if err != nil {
-			return nil, err
-		}
-
-		origNS := is.Namespace
-		if ns := hc.Spec.CommonBootImageNamespace; ns != nil && len(*ns) > 0 {
-			is.Namespace = *ns
-		}
-
-		is.Labels = operands.GetLabels(hc, util.AppComponentCompute)
-		imageStreamNames = append(imageStreamNames, is.Name)
-		return newImageStreamHandler(Client, Scheme, is, origNS), nil
+func processImageStreamFile(path string, dir fs.FS, logger log.Logger, hc *hcov1beta1.HyperConverged, Client client.Client, Scheme *runtime.Scheme) (operands.Operand, error) {
+	file, err := dir.Open(path)
+	if err != nil {
+		logger.Error(err, "Can't open the ImageStream yaml file", "file name", path)
+		return nil, err
 	}
 
-	return nil, nil
+	is := &imagev1.ImageStream{}
+	err = util.UnmarshalYamlFileToObject(file, is)
+	if err != nil {
+		return nil, err
+	}
+
+	if is.Kind != "ImageStream" {
+		logger.Info("error while processing the ImageStream files: the file is not an ImageStream manifest", "path", path, "actual kind", is.Kind)
+		return nil, nil
+	}
+
+	origNS := is.Namespace
+	if ns := hc.Spec.CommonBootImageNamespace; ns != nil && len(*ns) > 0 {
+		is.Namespace = *ns
+	}
+
+	is.Labels = operands.GetLabels(hc, util.AppComponentCompute)
+	imageStreamNames = append(imageStreamNames, is.Name)
+	return newImageStreamHandler(Client, Scheme, is, origNS), nil
 }
