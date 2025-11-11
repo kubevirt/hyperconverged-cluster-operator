bc575c121f4a4f68c459902b519574e64bffb76d
Oren Cohen <ocohen@redhat.com>
2025-11-11 11:46:46 +0200

    csv-merger: make Replaces field optional in the CSV
    When using an FBC catalog, the upgrade graph is not determined by the replaces,skips and skipRange fields in the CSV itself, but in the file-based catalog.
    Make the replaces-csv-version flag optional, so if it hasn't been provided, the generated CSV won't include the Replaces field at all.
    
    Signed-off-by: Oren Cohen <ocohen@redhat.com>

--- pkg/components/components.go
+++ pkg/components/components.go
@@ -777,7 +777,6 @@ type CSVBaseParams struct {
 	MetaDescription string
 	Description     string
 	Image           string
-	Replaces        string
 	Version         semver.Version
 	CrdDisplay      string
 }
@@ -923,7 +922,6 @@ func GetCSVBase(params *CSVBaseParams) *csvv1alpha1.ClusterServiceVersion {
 			Description: params.Description,
 			Keywords:    stringListToSlice("KubeVirt", "Virtualization"),
 			Version:     csvVersion.OperatorVersion{Version: params.Version},
-			Replaces:    params.Replaces,
 			Maintainers: []csvv1alpha1.Maintainer{
 				{
 					Name:  kubevirtProjectName,
