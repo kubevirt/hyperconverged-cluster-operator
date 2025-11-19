68b0a1104bd22aefa3572477648e93df80d247c1
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2025-11-19 13:47:46 +0200

    Make the builder image multi-arch
    This will allow developers working on windows or mac to use it.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- hack/builder/create-builder-image.sh
+++ hack/builder/create-builder-image.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Load build arguments directly from go.mod
 
+source ./hack/architecture.sh
+. ./hack/cri-bin.sh && export CRI_BIN=${CRI_BIN}
+
 script_dir="$(cd "$(dirname "$0")" && pwd -P)"
 IMAGE_NAME=${IMAGE_NAME:-hco-builder}
 
@@ -12,6 +15,15 @@ KUBEOPENAPI_VER=$(grep "k8s.io/kube-openapi => k8s.io/kube-openapi" go.mod | xar
 
 echo "Loaded from go.mod: K8S_VER=${K8S_VER}, KUBEOPENAPI_VER=${KUBEOPENAPI_VER}"
 
-podman build -t ${IMAGE_NAME} -f hack/builder/Dockerfile --build-arg K8S_VER=${K8S_VER} --build-arg KUBEOPENAPI_VER=${KUBEOPENAPI_VER} ${script_dir}
+if ${CRI_BIN} manifest exists "${IMAGE_NAME}"; then
+  ${CRI_BIN} manifest rm "${IMAGE_NAME}"
+fi
+${CRI_BIN} manifest create "${IMAGE_NAME}"
 
+for arch in ${ARCHITECTURES}; do
+  ${CRI_BIN} build --platform="linux/${arch}" -t ${IMAGE_NAME}-${arch} -f hack/builder/Dockerfile --build-arg K8S_VER=${K8S_VER} --build-arg KUBEOPENAPI_VER=${KUBEOPENAPI_VER} ${script_dir}
+  ./hack/retry.sh 3 10 "${CRI_BIN} push ${IMAGE_NAME}-${arch}"
+  ${CRI_BIN} manifest add "${IMAGE_NAME}" "${IMAGE_NAME}-${arch}"
+done
 
+./hack/retry.sh 3 10 "${CRI_BIN} manifest push ${IMAGE_NAME}"
