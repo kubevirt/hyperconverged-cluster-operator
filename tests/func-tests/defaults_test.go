c6637d001513f4d1455a7b01ebc186b1a5912b20
Shelly Kagan <skagan@redhat.com>
2026-02-12 16:36:58 +0200

    tests: Include functional and unit test coverage for IncrementalBackup feature gate
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- tests/func-tests/defaults_test.go
+++ tests/func-tests/defaults_test.go
@@ -76,6 +76,7 @@ var _ = Describe("Check Default values", Label("defaults"), Serial, func() {
 			DeclarativeHotplugVolumes:      ptr.To(false),
 			VideoConfig:                    ptr.To(true),
 			ObjectGraph:                    ptr.To(false),
+			IncrementalBackup:              ptr.To(false),
 		}
 
 		DescribeTable("Check that featureGates defaults are behaving as expected", func(ctx context.Context, path string) {
@@ -98,6 +99,7 @@ var _ = Describe("Check Default values", Label("defaults"), Serial, func() {
 			Entry("when removing /spec/featureGates/declarativeHotplugVolumes", "/spec/featureGates/declarativeHotplugVolumes"),
 			Entry("when removing /spec/featureGates/videoConfig", "/spec/featureGates/videoConfig"),
 			Entry("when removing /spec/featureGates/objectGraph", "/spec/featureGates/objectGraph"),
+			Entry("when removing /spec/featureGates/incrementalBackup", "/spec/featureGates/incrementalBackup"),
 			Entry("when removing /spec/featureGates", "/spec/featureGates"),
 			Entry("when removing /spec", "/spec"),
 		)
