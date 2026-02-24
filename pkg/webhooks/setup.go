f1f85659a625f528bb056e135615ff4e1be2e27f
Nahshon Unna Tsameret <nunnatsa@redhat.com>
2026-02-24 18:13:14 +0200

    Register v1 mutating and validating webhooks
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- pkg/webhooks/setup.go
+++ pkg/webhooks/setup.go
@@ -26,15 +26,19 @@ func SetupWebhookWithManager(mgr ctrl.Manager, isOpenshift bool) error {
 
 	decoder := admission.NewDecoder(mgr.GetScheme())
 
-	whHandler := validator.NewWebhookHandler(logger, mgr.GetClient(), decoder, operatorNsEnv, isOpenshift)
+	v1Beta1WHHandler := validator.NewWebhookV1Beta1Handler(logger, mgr.GetClient(), decoder, operatorNsEnv, isOpenshift)
+	v1WHHandler := validator.NewWebhookHandler(logger, mgr.GetClient(), decoder, operatorNsEnv, isOpenshift, v1Beta1WHHandler)
 	nsMutator := mutator.NewNsMutator(mgr.GetClient(), decoder, operatorNsEnv)
-	hyperConvergedMutator := mutator.NewHyperConvergedMutator(mgr.GetClient(), decoder)
+	v1HCMutator := mutator.NewHyperConvergedMutator(mgr.GetClient(), decoder)
+	v1Beta1HCMutator := mutator.NewHyperConvergedV1Beta1Mutator(mgr.GetClient(), decoder)
 
 	srv := mgr.GetWebhookServer()
 
 	srv.Register(hcoutil.HCONSWebhookPath, &webhook.Admission{Handler: nsMutator})
-	srv.Register(hcoutil.HCOMutatingWebhookPath, &webhook.Admission{Handler: hyperConvergedMutator})
-	srv.Register(hcoutil.HCOWebhookPath, &webhook.Admission{Handler: whHandler})
+	srv.Register(hcoutil.HCOV1Beta1MutatingWebhookPath, &webhook.Admission{Handler: v1Beta1HCMutator})
+	srv.Register(hcoutil.HCOV1MutatingWebhookPath, &webhook.Admission{Handler: v1HCMutator})
+	srv.Register(hcoutil.HCOV1Beta1WebhookPath, &webhook.Admission{Handler: v1Beta1WHHandler})
+	srv.Register(hcoutil.HCOV1WebhookPath, &webhook.Admission{Handler: v1WHHandler})
 
 	return nil
 }
