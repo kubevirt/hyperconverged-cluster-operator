c639fa72d924de5e5c6b5625590067138ad6b887
Nahshon Unna Tsameret <nunnatsa@redhat.com>
2026-03-25 16:56:04 +0200

    For own endpoins, fix TLS 1.3 setings
    For the webhook and the monitoring endpoints, if the minimum TLS version
    is 1.3, don't set the ciphesuitees, as this setting is not supported by
    golang.
    
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- pkg/internal/tlssecprofile/tls_security_profile_test.go
+++ pkg/internal/tlssecprofile/tls_security_profile_test.go
@@ -512,12 +512,7 @@ var _ = Describe("TLS Security Profile", func() {
 			Expect(err).ToNot(HaveOccurred())
 
 			Expect(config.MinVersion).To(Equal(uint16(tls.VersionTLS13)))
-			expectedTLSCiphers := []uint16{
-				tls.TLS_AES_128_GCM_SHA256,
-				tls.TLS_AES_256_GCM_SHA384,
-				tls.TLS_CHACHA20_POLY1305_SHA256,
-			}
-			Expect(config.CipherSuites).To(Equal(expectedTLSCiphers))
+			Expect(config.CipherSuites).To(BeEmpty())
 		})
 
 		It("should use intermediate profile if both HCO and APIServer profiles are not provided", func() {
