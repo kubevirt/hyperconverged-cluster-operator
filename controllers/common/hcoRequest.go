47ff3fa7b65b71715e93eec81d3121e1ca4d3a6d
Nahshon Unna Tsameret <nunnatsa@redhat.com>
2026-05-06 16:32:34 +0300

    use v1 in the HcoRequest type
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- controllers/common/hcoRequest.go
+++ controllers/common/hcoRequest.go
@@ -6,22 +6,22 @@ import (
 	"github.com/go-logr/logr"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	hcov1beta1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1beta1"
+	hcov1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1"
 )
 
 // hcoRequest - gather data for a specific request
 type HcoRequest struct {
-	reconcile.Request                                     // inheritance of operator request
-	Logger                     logr.Logger                // request logger
-	Conditions                 HcoConditions              // in-memory conditions
-	Ctx                        context.Context            // context of this request, to be use for any other call
-	Instance                   *hcov1beta1.HyperConverged // the current state of the CR, as read from K8s
-	UpgradeMode                bool                       // copy of the reconciler upgrade mode
-	ComponentUpgradeInProgress bool                       // if in upgrade mode, accumulate the component upgrade status
-	Dirty                      bool                       // is something was changed in the CR
-	StatusDirty                bool                       // is something was changed in the CR's Status
-	HCOTriggered               bool                       // if the request got triggered by a direct modification on HCO CR
-	Upgradeable                bool                       // if all the operands are upgradeable
+	reconcile.Request                                // inheritance of operator request
+	Logger                     logr.Logger           // request logger
+	Conditions                 HcoConditions         // in-memory conditions
+	Ctx                        context.Context       // context of this request, to be use for any other call
+	Instance                   *hcov1.HyperConverged // the current state of the CR, as read from K8s
+	UpgradeMode                bool                  // copy of the reconciler upgrade mode
+	ComponentUpgradeInProgress bool                  // if in upgrade mode, accumulate the component upgrade status
+	Dirty                      bool                  // is something was changed in the CR
+	StatusDirty                bool                  // is something was changed in the CR's Status
+	HCOTriggered               bool                  // if the request got triggered by a direct modification on HCO CR
+	Upgradeable                bool                  // if all the operands are upgradeable
 }
 
 func NewHcoRequest(ctx context.Context, request reconcile.Request, log logr.Logger, upgradeMode, hcoTriggered bool) *HcoRequest {
