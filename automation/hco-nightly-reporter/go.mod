383accb2890a83891809d321f58f3b99aaba7ae0
Nahshon Unna Tsameret <60659093+nunnatsa@users.noreply.github.com>
2026-05-19 12:59:03 +0300

    The nightly notifier also reports OCP nightly build results (#4263)
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- automation/hco-nightly-reporter/go.mod
+++ automation/hco-nightly-reporter/go.mod
@@ -1,10 +1,7 @@
 module github.com/hyperconverged-cluster-operator/automation/hco-nightly-reporter
 
-go 1.25
+go 1.26
 
-require github.com/slack-go/slack v0.17.3
+require github.com/slack-go/slack v0.23.1
 
-require (
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/stretchr/testify v1.11.1 // indirect
-)
+require github.com/gorilla/websocket v1.5.3 // indirect
--- automation/hco-nightly-reporter/main.go
+++ automation/hco-nightly-reporter/main.go
@@ -7,16 +7,31 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/slack-go/slack"
 )
 
-const (
-	basicPrawURL        = "https://storage.googleapis.com/kubevirt-prow/logs/periodic-hco-push-nightly-build-main"
-	latestBuildURL      = basicPrawURL + "/latest-build.txt"
-	finishedURLTemplate = basicPrawURL + "/%s/finished.json"
-	jobURLTemplate      = basicPrawURL + "/%s/prowjob.json"
+type jobConfig struct {
+	name    string
+	baseURL string
+}
+
+var jobs = []jobConfig{
+	{
+		name:    "Nightly build on kubevirtci",
+		baseURL: "https://storage.googleapis.com/kubevirt-prow/logs/periodic-hco-push-nightly-build-main",
+	},
+	{
+		name:    "Nightly deploy on OCP",
+		baseURL: "https://storage.googleapis.com/test-platform-results/logs/periodic-ci-kubevirt-hyperconverged-cluster-operator-main-hco-e2e-deploy-nightly-main-aws",
+	},
+}
+
+var (
+	successEmoji = slack.NewRichTextSectionEmojiElement("solid-success", 3, nil)
+	failedEmoji  = slack.NewRichTextSectionEmojiElement("failed", 3, nil)
 )
 
 type finished struct {
@@ -24,10 +39,6 @@ type finished struct {
 	Passed    bool   `json:"passed"`
 	Result    string `json:"result"`
 	Revision  string `json:"revision"`
-}
-
-func (f finished) getBuildTime() time.Time {
-	return time.Unix(f.Timestamp, 0).UTC()
 }
 
 var (
@@ -61,60 +72,88 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 
-	blocks, jobURL, err := generateMessage(ctx)
+	blocks, err := generateMessage(ctx)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
 	err = sendMessageToSlackChannel(blocks)
-
 	if err != nil {
-		writeSendError(err, jobURL)
+		fmt.Fprintln(os.Stderr, "failed to send the message to the channel; ", err.Error())
+		if serr, ok := err.(slack.SlackErrorResponse); ok {
+			for _, msg := range serr.ResponseMetadata.Messages {
+				fmt.Fprintln(os.Stderr, msg)
+			}
+		}
 		os.Exit(1)
 	}
 
 	fmt.Println("Successfully sent message to the channel")
 }
 
-func writeSendError(err error, jobURL string) {
-	fmt.Fprintln(os.Stderr, "failed to send the message to the channel; ", err.Error())
-	if serr, ok := err.(slack.SlackErrorResponse); ok {
-		for _, msg := range serr.ResponseMetadata.Messages {
-			fmt.Fprintln(os.Stderr, msg)
-		}
-	}
-
-	if len(jobURL) > 0 {
-		fmt.Fprintln(os.Stderr, "job URL: ", jobURL)
-	}
-}
-
-func generateMessage(ctx context.Context) ([]slack.Block, string, error) {
+func generateMessage(ctx context.Context) ([]slack.Block, error) {
 	client := http.DefaultClient
 	client.Timeout = time.Second * 3
 
-	latestBuild, err := getLatestBuild(ctx, client)
-	if err != nil {
-		return nil, "", fmt.Errorf("failed to latest job ID; %s", err.Error())
+	var allBlocks []slack.Block
+	needMention := false
+	successCount := 0
+
+	for i, job := range jobs {
+		if i > 0 {
+			allBlocks = append(allBlocks, slack.NewDividerBlock())
+		}
+
+		blocks, shouldMention, err := generateJobMessage(ctx, client, job)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to get status for %q: %s\n", job.name, err.Error())
+			blocks = generateErrorMessage(job.name, err)
+			shouldMention = true
+		} else {
+			successCount++
+		}
+
+		allBlocks = append(allBlocks, blocks...)
+		if shouldMention {
+			needMention = true
+		}
 	}
 
-	buildStatus, err := getBuildStatus(ctx, latestBuild)
+	if successCount == 0 && len(allBlocks) == 0 {
+		return nil, fmt.Errorf("failed to fetch status for all jobs")
+	}
+
+	if needMention {
+		allBlocks = append(allBlocks, generateMentionBlock())
+	}
+
+	return allBlocks, nil
+}
+
+func generateJobMessage(ctx context.Context, client *http.Client, job jobConfig) ([]slack.Block, bool, error) {
+	latestBuild, err := getLatestBuild(ctx, client, job.baseURL)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to fetch the build status; %s", err.Error())
+		return nil, false, fmt.Errorf("failed to get latest job ID; %s", err.Error())
+	}
+
+	buildStatus, err := getBuildStatus(ctx, client, latestBuild, job.baseURL)
+	if err != nil {
+		return nil, false, fmt.Errorf("failed to fetch the build status; %s", err.Error())
 	}
 
 	buildTime := time.Unix(buildStatus.Timestamp, 0).UTC()
 	if time.Since(buildTime).Hours() > 24 {
-		return generateNoBuildMessage(buildTime), "", nil
+		return generateNoBuildMessage(job.name, buildTime), true, nil
 	}
 
-	jobURL, err := getJob(ctx, latestBuild)
+	jobURL, err := getJob(ctx, client, latestBuild, job.baseURL)
 	if err != nil {
-		return nil, "", fmt.Errorf("failed to fetch the job info; %s", err.Error())
+		return nil, false, fmt.Errorf("failed to fetch the job info; %s", err.Error())
 	}
 
-	return generateStatusMessage(buildStatus, buildTime, jobURL), jobURL, nil
+	blocks, shouldMention := generateStatusMessage(job.name, buildStatus, buildTime, jobURL)
+	return blocks, shouldMention, nil
 }
 
 func sendMessageToSlackChannel(blocks []slack.Block) error {
@@ -129,53 +168,62 @@ func generateMentionBlock() slack.Block {
 	))
 }
 
-func generateNoBuildMessage(buildTime time.Time) []slack.Block {
+func generateErrorMessage(jobName string, fetchErr error) []slack.Block {
 	return []slack.Block{
-		slack.NewRichTextBlock("status", slack.NewRichTextSection(
-			slack.NewRichTextSectionEmojiElement("failed", 3, nil),
+		slack.NewRichTextBlock("", slack.NewRichTextSection(
+			failedEmoji,
 			slack.NewRichTextSectionTextElement(
-				" Nightly build wasn't run today", nil,
+				fmt.Sprintf(" %s: failed to fetch status: %s", jobName, fetchErr.Error()), nil,
 			),
 		)),
-		slack.NewRichTextBlock("last-build-time", slack.NewRichTextSection(
+	}
+}
+
+func generateNoBuildMessage(jobName string, buildTime time.Time) []slack.Block {
+	return []slack.Block{
+		slack.NewRichTextBlock("", slack.NewRichTextSection(
+			failedEmoji,
+			slack.NewRichTextSectionTextElement(
+				fmt.Sprintf(" %s wasn't run today", jobName), nil,
+			),
+		)),
+		slack.NewRichTextBlock("", slack.NewRichTextSection(
 			slack.NewRichTextSectionTextElement("Last build was at ", nil),
 			slack.NewRichTextSectionDateElement(buildTime.UTC().Unix(), "{date_long_full} at {time}, {ago}", nil, nil),
 		)),
-		generateMentionBlock(),
 	}
 }
 
-func generateStatusMessage(buildStatus *finished, buildTime time.Time, jobURL string) []slack.Block {
-	var status, emoji string
+func generateStatusMessage(jobName string, buildStatus *finished, buildTime time.Time, jobURL string) ([]slack.Block, bool) {
+	var (
+		status string
+		emoji  *slack.RichTextSectionEmojiElement
+	)
 	if buildStatus.Passed {
 		status = "passed"
-		emoji = "solid-success"
+		emoji = successEmoji
 	} else {
 		status = "failed"
-		emoji = "failed"
+		emoji = failedEmoji
 	}
 
 	blocks := []slack.Block{
-		slack.NewRichTextBlock("status", slack.NewRichTextSection(
-			slack.NewRichTextSectionEmojiElement(emoji, 3, nil),
+		slack.NewRichTextBlock("", slack.NewRichTextSection(
+			emoji,
 			slack.NewRichTextSectionTextElement(
-				" Nightly build ", nil,
+				fmt.Sprintf(" %s ", jobName), nil,
 			),
-			slack.NewRichTextSectionTextElement(" ", nil),
 			slack.NewRichTextSectionLinkElement(jobURL, status, &slack.RichTextSectionTextStyle{Bold: true}),
 			slack.NewRichTextSectionTextElement(", at ", nil),
-			slack.NewRichTextSectionDateElement(buildTime.UTC().Unix(), "{date_long} at {time}", nil, nil),
+			slack.NewRichTextSectionDateElement(buildTime.UTC().Unix(), "{date_long_full} at {time}", nil, nil),
 		)),
 	}
 
-	if !buildStatus.Passed {
-		blocks = append(blocks, generateMentionBlock())
-	}
-	return blocks
+	return blocks, !buildStatus.Passed
 }
 
-func getLatestBuild(ctx context.Context, client *http.Client) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, latestBuildURL, nil)
+func getLatestBuild(ctx context.Context, client *http.Client, baseURL string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, baseURL+"/latest-build.txt", nil)
 	if err != nil {
 		return "", err
 	}
@@ -186,26 +234,38 @@ func getLatestBuild(ctx context.Context, client *http.Client) (string, error) {
 	}
 
 	defer resp.Body.Close()
+	if err = checkHTTPStatusCode(resp); err != nil {
+		return "", fmt.Errorf("latest build request failed; %w", err)
+	}
 
 	latestBuildBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
+
+	latestBuild := strings.TrimSpace(string(latestBuildBytes))
+	if latestBuild == "" {
+		return "", fmt.Errorf("latest build is empty")
+	}
+
 	return string(latestBuildBytes), nil
 }
 
-func getBuildStatus(ctx context.Context, latestBuild string) (*finished, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(finishedURLTemplate, latestBuild), nil)
+func getBuildStatus(ctx context.Context, client *http.Client, latestBuild string, baseURL string) (*finished, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s/finished.json", baseURL, latestBuild), nil)
 	if err != nil {
 		return nil, err
 	}
 
-	finishedResp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	finishedResp, err := client.Do(req.WithContext(ctx))
 	if err != nil {
 		return nil, err
 	}
 
 	defer finishedResp.Body.Close()
+	if err = checkHTTPStatusCode(finishedResp); err != nil {
+		return nil, fmt.Errorf("failed to get finished response; %w", err)
+	}
 
 	f := &finished{}
 	dec := json.NewDecoder(finishedResp.Body)
@@ -215,18 +275,21 @@ func getBuildStatus(ctx context.Context, latestBuild string) (*finished, error) 
 	return f, nil
 }
 
-func getJob(ctx context.Context, latestBuild string) (string, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(jobURLTemplate, latestBuild), nil)
+func getJob(ctx context.Context, client *http.Client, latestBuild string, baseURL string) (string, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s/prowjob.json", baseURL, latestBuild), nil)
 	if err != nil {
 		return "", err
 	}
 
-	jobResp, err := http.DefaultClient.Do(req.WithContext(ctx))
+	jobResp, err := client.Do(req.WithContext(ctx))
 	if err != nil {
 		return "", err
 	}
 
 	defer jobResp.Body.Close()
+	if err = checkHTTPStatusCode(jobResp); err != nil {
+		return "", fmt.Errorf("failed to get job details; %w", err)
+	}
 
 	job := struct {
 		Status struct {
@@ -239,4 +302,13 @@ func getJob(ctx context.Context, latestBuild string) (string, error) {
 		return "", err
 	}
 	return job.Status.URL, nil
+}
+
+func checkHTTPStatusCode(resp *http.Response) error {
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(io.LimitReader(resp.Body, 512))
+		return fmt.Errorf("HTTP status is not OK; %s: %s", resp.Status, strings.TrimSpace(string(body)))
+	}
+
+	return nil
 }
