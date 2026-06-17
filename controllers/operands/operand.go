feb103332278c68c9b6a83f7c8920d55ffca23df
Nahshon Unna Tsameret <nunnatsa@redhat.com>
2026-06-17 16:25:20 +0300

    fix CNV-90394: many resources are not deleted when deleting the HCO CR
    After moving the operands creation to be static, we introduced a bug
    where most of the resources created by HCO are not deleted when HCO is
    removed.
    
    The reason for that is that the old dynamic addition of operand also
    added the resource to a list in the operandHandler, to be used for the
    deletion. But when we stopped using the dynamic code, the resources are
    not listed anymore, and are not deleted.
    
    This commit fixes this issue by re-creating the list.
    
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- controllers/operands/operand.go
+++ controllers/operands/operand.go
@@ -54,6 +54,12 @@ type Reseter interface {
 	Reset()
 }
 
+// ManualDeletionMarker is a marker interface, to exclude specific handlers from batch deletion
+// tracking. these are handled separately.
+type ManualDeletionMarker interface {
+	ManualDeletionMark()
+}
+
 type GetHandler func(log.Logger, client.Client, *runtime.Scheme, *hcov1.HyperConverged) (Operand, error)
 type GetHandlers func(log.Logger, client.Client, *runtime.Scheme, *hcov1.HyperConverged, fs.FS) ([]Operand, error)
 
