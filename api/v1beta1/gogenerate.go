eb2bf07be3fa7501e959eac28b1eae8cfc2a5734
Nahshon Unna Tsameret <nunnatsa@redhat.com>
2026-07-02 10:42:29 +0300

    explicitly don't auto-convert deprecated FGs
    We already make the pkg/featuregatedetails/feature-gates.json file to be
    the source of truth for the feature gates, instead of the v1beta1
    `HyperConvergedFeatureGates` field. But the fg-json-generator tool still
    uses the old source. In addition, we no longer auto generating the json
    file.
    
    This commit modifies the fg-json-generator:
    * rename to the tool to `fg-conversion-generator`, to reflect
      the changes
    * remove all the json-generation related code from the tool
    * add comments in the generated file, for better readability.
    * sort the feature gates in the generated file (by phase,
      then by name), for better readability.
    * explicitly not convert deprecated FGs. Add comments instead.
    
    Signed-off-by: Nahshon Unna Tsameret <nunnatsa@redhat.com>

--- api/v1beta1/gogenerate.go
+++ api/v1beta1/gogenerate.go
@@ -1,3 +1,3 @@
 package v1beta1
 
-//go:generate go run ../../tools/fg-json-generator --conversion --out=./zz_generated.featuregates_conversion.go --in=./hyperconverged_types.go
+//go:generate go run ../../tools/fg-conversion-generator --out=./zz_generated.featuregates_conversion.go --in=./hyperconverged_types.go
