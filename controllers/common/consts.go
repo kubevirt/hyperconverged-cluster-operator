c97161c14d5e0c5c56c188c375cc36f26f8dd73d
Nir Dothan <ndothan@redhat.com>
2026-07-26 15:23:03 +0300

    controllers: common: Mv func from const to new file
    The fuction was mislocated in file dedicated to consts.
    Relocated to a new file.
    
    Signed-off-by: Nir Dothan <ndothan@redhat.com>

--- controllers/common/consts.go
+++ controllers/common/consts.go
@@ -1,16 +1,8 @@
 package common
 
 import (
-	"k8s.io/utils/ptr"
-
-	hcov1 "github.com/kubevirt/hyperconverged-cluster-operator/api/v1"
 	"github.com/kubevirt/hyperconverged-cluster-operator/pkg/util"
 )
-
-// ShouldDeployNetworkResourcesInjector checks if network-resources-injector should be deployed
-func ShouldDeployNetworkResourcesInjector(hc *hcov1.HyperConverged) bool {
-	return ptr.Deref(hc.Spec.Deployment.DeployNetworkResourcesInjector, true)
-}
 
 const (
 	ReconcileCompleted        = "ReconcileCompleted"
